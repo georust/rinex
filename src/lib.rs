0608f7192991075726b40b1601a0c5815c804646
gwbres <guillaume.bressaix@gorgy-timing.fr>
2021-12-17 13:39:49 +0100

    working on lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
-//! TODO
+//! RINEX library to parse and analyze RINEX files.
+//! Current supported RINEX Version is 2.10.
+//! Supported the following RINEX files format
 //! 
 //! url:
 
@@ -17,7 +19,7 @@ enum VersionFormatError {
     ParseIntError(#[from] std::num::ParseIntError),
 }
 
-/// Describes all known GNSS constellations
+/// Describes all known `GNSS constellations`
 #[derive(Clone, PartialEq, Debug)]
 pub enum Constellation {
     GPS,
@@ -25,6 +27,7 @@ pub enum Constellation {
     Beidou,
     QZSS,
     Galileo,
+    Mixed, // mixed constellation records
 }
 
 #[derive(Error, Debug)]
@@ -46,30 +49,60 @@ impl std::str::FromStr for Constellation {
             Ok(Constellation::QZSS)
         } else if s.starts_with("C") {
             Ok(Constellation::Beidou)
+        } else if s.starts_with("M") {
+            Ok(Constellation::Mixed)
         } else {
             Err(ConstellationError::UnknownConstellation(s.to_string()))
         }
     }
 }
 
-/// Describes all known Rinex file format 
-enum Format {
+/// Describes all known RINEX file types
+#[derive(Debug)]
+enum DataType {
     Observation,
+/*
+    Navigation,
     Meteo,
     MeteoData,
     GpsEphemeris,
     GloEphemeris,
     GalEphemeris,
+*/
 }
 
-/// Header Parsing / formatting errors
+#[derive(Error, Debug)]
+enum DataTypeError {
+    #[error("Unknown RINEX data '{0}'")]
+    UnknownDataType(String),
+}
+
+impl std::str::FromStr for DataType {
+    type Err = DataTypeError;
+    fn from_str (s: &str) -> Result<Self, Self::Err> {
+        if s.starts_with("OBSERVATION DATA") {
+            Ok(DataType::Observation)
+        } else {
+            Err(DataTypeError::UnknownDataType(String::from(s)))
+        }
+    }
+}
+
+#[derive(Error, Debug)]
 enum HeaderError {
-    HeaderError,
-    VersionNotSupported,
-    UnknownFormat,
+    #[error("RINEX version is not supported '{0}'")]
+    VersionNotSupported(String),
+    #[error("Non supported header format")]
+    FmtError,
+    #[error("Unknown RINEX file type '{0}'")]
+    UnknownRinexType(String),
+    #[error("Unknown GNSS Constellation '{0}'")]
+    UnknownConstellation(#[from] ConstellationError),
+    #[error("Unknown Data Type '{0}'")]
+    DataTypeError(#[from] DataTypeError),
 }
 
-/// Receiver used in recording
+/// GNSS receiver description
 #[derive(Debug, PartialEq)]
 struct Rcvr {
     model: String, 
@@ -96,23 +129,27 @@ impl std::str::FromStr for Rcvr {
     }
 }
 
-/// Antenna description
+/// Antenna description 
+#[derive(Debug)]
 struct Antenna {
-    model: &'static str,
-    sn: &'static str, // serial #
+    model: String,
+    sn: String, // serial #
     coords: geo_types::Point<f32>, // ANT approx. coordinates
 }
 
-/// GnssTime: `UTC` time with associated `constellation`
+/// GnssTime struct is a `UTC` time 
+/// realized from given associated `GNSS constellation`
+#[derive(Debug)]
 struct GnssTime {
-    time: chrono::Utc, /// UTC time
-    constellation: Constellation,
+    utc: chrono::DateTime<chrono::Utc>, /// UTC time
+    gnss: Constellation,
 }
 
-/// Describes file header
-struct RinexHeader {
-    version: &'static str, // Rinex format version
-    fmt: Format, // file format (observation, data..)
+/// Describes RINEX file header
+#[derive(Debug)]
+struct Header {
+    version: String, // Rinex format version
+    data: DataType, // file format (observation, data..)
     gnss: Constellation, // GNSS constellation being used
     marker_name: &'static str, // marker name 
     marker_number: &'static str, // marker number
@@ -120,30 +157,74 @@ struct RinexHeader {
     station: Option<&'static str>, // station label
     observer: &'static str, // observer label
     agency: &'static str, // observer/agency
-    rcvr: Rcvr, // receiver used for recording
-    ant: Antenna, // ant used for recording
+    rcvr: Rcvr, // receiver used for this recording
+    ant: Antenna, // antenna used for this recording
     coords: geo_types::Point<f32>, // station approx. coords
     wavelengths: Option<(u32,u32)>, // L1/L2 wavelengths
-    nb_obsverations: u64,
+    nb_observations: u64,
     //observations: Observation,
     sampling_interval: f32, // sampling
     first_epoch: GnssTime, // date of first observation
-    last_epoch: GnssTime, // date of last observation
+    last_epoch: Option<GnssTime>, // date of last observation
     comments: Option<&'static str>, // optionnal comments
     epoch_corrected: bool, // true if epochs are already corrected
     gps_utc_delta: Option<u32>, // optionnal GPS / UTC time difference
     sat_number: Option<u32>, // nb of sat for which we have data
 }
 
-//match scan_fmt! (&content, "RCR = {} {} {d}", String, String,)
-
-/// RinexHeader displayer
-impl std::fmt::Display for RinexHeader {
+/// Header displayer
+impl std::fmt::Display for Header {
     fn fmt (&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         write!(f,
             "RINEX Version: {}",
             self.version
         )
+    }
+}
+
+impl std::str::FromStr for Header {
+    type Err = HeaderError;
+    fn from_str (s: &str) -> Result<Self, Self::Err> {
+        let (version, fmt1, fmt2, gnss) = match scan_fmt!(s, 
+            "{[0-9].[0-9][0-9]} {} {} {}", String, String, String, String)
+        {
+            (Some(version), Some(fmt1), Some(fmt2), Some(gnss)) => (version,fmt1,fmt2,gnss),
+            _ => return Err(HeaderError::FmtError),
+        };
+
+        Ok(Header{
+            version,
+            data: DataType::from_str(&(String::from(&fmt1).to_owned() + &fmt2))?,
+            gnss: Constellation::from_str(&gnss)?,
+            marker_name: "",
+            marker_number: "",
+            station: None,
+            observer: "",
+            agency: "",
+            rcvr: Rcvr {
+                model: String::from("test"),
+                sn: String::from("test"),
+                firmware: String::from("test"),
+            },
+            ant: Antenna {
+                model: String::from("test"),
+                sn: String::from("test"),
+                coords: geo_types::Point::new(0.0, 0.0),
+            },
+            coords: geo_types::Point::new(0.0,0.0),
+            wavelengths: None,
+            nb_observations: 0,
+            sampling_interval: 0.0,
+            first_epoch: GnssTime {
+                utc: chrono::Utc::now(),
+                gnss: Constellation::GPS,
+            },
+            last_epoch: None,
+            epoch_corrected: false,
+            comments: None,
+            gps_utc_delta: None,
+            sat_number: None,
+        })
     }
 }
 
@@ -205,6 +286,7 @@ mod test {
     use super::*;
 
     #[test]
+    /// tests version support identification tool
     fn test_version_tool() {
         assert_eq!(version_is_supported("a.b").is_err(), true); // fmt error
         assert_eq!(version_is_supported("1.0").unwrap(), true); // OK basic
@@ -214,16 +296,46 @@ mod test {
     }
 
     #[test]
+    /// tests Rcvr object fromStr method
     fn rcvr_from_str() {
-        /* correct format #1 */
-        let rcvr = Rcvr::from_str("82205 LEICA RS500 4.20/1.39");
-        println!("{:?}", rcvr);
+        /* standard format #1 */
         assert_eq!(
-            rcvr.unwrap(),
+            Rcvr::from_str("82205               LEICA RS500         4.20/1.39")
+                .unwrap(),
             Rcvr{
                 sn: String::from("82205"),
                 model: String::from("LEICA RS500"),
                 firmware: String::from("4.20/1.39")
             });
+        
+        /* faulty whitespaces but passes */
+        assert_eq!(
+            Rcvr::from_str("82205 LEICA RS500 4.20/1.39")
+                .unwrap(),
+            Rcvr{
+                sn: String::from("82205"),
+                model: String::from("LEICA RS500"),
+                firmware: String::from("4.20/1.39")
+            });
+    }
+
+    #[test]
+    /// tests Header fromStr method
+    fn header_from_str() {
+        // should crash triple digit
+        assert_eq!(
+            Header::from_str("2.100 OBSERVATION DATA G (GPS)").is_err(),
+            true);
+        // should crash missing .XX
+        assert_eq!(
+            Header::from_str("2. OBSERVATION DATA G (GPS)").is_err(),
+            true);
+        // should crash missing .XX
+        assert_eq!(
+            Header::from_str("2 OBSERVATION DATA G (GPS)").is_err(),
+            true);
+        // should pass
+        let hd = Header::from_str("2.00 OBSERVATION DATA G (GPS)");
+        println!("{:?}", hd);
     }
 }
