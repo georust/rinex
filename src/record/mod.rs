7027f9372d43360fc9597daaacec0912998f836a
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2021-12-29 15:43:37 +0100

    improved RecordItem

--- src/record/mod.rs
+++ src/record/mod.rs
@@ -12,16 +12,9 @@ pub enum RinexRecord {
 type Epoch = chrono::NaiveDateTime;
 
 #[derive(Debug)]
-pub enum RecordItemType {
-    Float64,
-    //FixedPoint(String),
-    //Epoch,
-}
-
-#[derive(Debug)]
-struct RecordItem {
-    item_type: RecordItemType,
-    item_value: String,
+pub enum RecordItem {
+    Float64(f64),
+    Epoch(Epoch),
 }
 
 #[derive(Error, Debug)]
@@ -30,19 +23,38 @@ pub enum RecordItemError {
     ParseIntError(#[from] std::num::ParseIntError),
     #[error("failed to parse float value")]
     ParseFloatError(#[from] std::num::ParseFloatError),
+    #[error("unknown type descriptor \"{0}\"")]
+    UnknownTypeDescriptor(String),
 }
 
 impl RecordItem {
-    fn from_string (item_type: RecordItemType, content: &str) 
+    fn from_string (type_descriptor: &str, content: &str) 
             -> Result<RecordItem, RecordItemError> 
     {
-        let item_value = match item_type {
-            RecordItemType::Float64 => f64::from_str(&content.replace("D","e"))?.to_string(),
-        };
-        Ok(RecordItem {
-            item_type,
-            item_value
-        })
+        match type_descriptor {
+            "f64" => {
+                Ok(RecordItem::Float64(
+                    f64::from_str(&content.replace("D","e"))?))
+             },
+             "epoch" => {
+                let items: Vec<&str> = content.split_ascii_whitespace()
+                    .collect();
+                let (mut y,mon,day,h,min,s): (i32,u32,u32,u32,u32,f64) =
+                    (i32::from_str_radix(items[0], 10)?,
+                    u32::from_str_radix(items[1], 10)?,
+                    u32::from_str_radix(items[2], 10)?,
+                    u32::from_str_radix(items[3], 10)?,
+                    u32::from_str_radix(items[4], 10)?,
+                    f64::from_str(items[5])?);
+                if y < 100 {
+                    y += 2000 // 2 digit nb case
+                }
+                Ok(RecordItem::Epoch(
+                    chrono::NaiveDate::from_ymd(y,mon,day)
+                        .and_hms(h,min,s as u32)))
+            },
+            _ => Err(RecordItemError::UnknownTypeDescriptor(type_descriptor.to_string())),
+        }
     }
 }
 
