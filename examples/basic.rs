e802f4b89a144df050d44295b61cbf3f43f81074
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2022-01-01 10:07:58 +0100

    basic example

---
+++ examples/basic.rs
@@ -0,0 +1,14 @@
+use rinex::*;
+use std::path::PathBuf;
+use rinex::constellation::*;
+
+fn main() {
+    println!("RINEX: example: basic");
+    let rinex = Rinex::from_file(&PathBuf::from("navsmall1.rinex")).unwrap();
+    let header = rinex.get_header();
+    println!("{:#?}", header);
+    assert_eq!(header.get_rinex_type(), RinexType::NavigationMessage);
+    assert_eq!(header.get_constellation(), constellation::Constellation::Mixed);
+    println!("{:#?}", header.get_rinex_version());
+    println!("Record size: {}", rinex.len());
+}
