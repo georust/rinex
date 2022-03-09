7153983800a17b5ed8427113e34b6996414b29d2
gwbres <guillaume.bressaix@gorgy-timing.fr>
2022-03-09 15:19:26 +0100

    improved types naming

--- examples/basic.rs
+++ examples/basic.rs
@@ -5,7 +5,7 @@ fn main() {
     println!("RINEX: example: basic");
     let rinex = Rinex::from_file(&PathBuf::from("navsmall1.rinex")).unwrap();
     println!("{:#?}", rinex.header);
-    assert_eq!(rinex.header.rinex_type, RinexType::NavigationMessage);
+    assert_eq!(rinex.header.rinex_type, Type::NavigationMessage);
     assert_eq!(rinex.header.constellation, constellation::Constellation::Mixed);
     println!("{:#?}", rinex.header.version);
     println!("Record size: {:#?}", rinex.record);
