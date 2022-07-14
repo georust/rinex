b9206b2507d1d4e2115db9a27c834673ef7d01e1
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2022-07-14 17:01:17 +0200

    add more tests

---
+++ rinex/tests/clocks.rs
@@ -0,0 +1,30 @@
+#[cfg(test)]
+mod test {
+    use rinex::*;
+    use std::str::FromStr;
+    use std::process::Command;
+    //#[test]
+    fn v3_demo() {
+        let test_resource = 
+            env!("CARGO_MANIFEST_DIR").to_owned() 
+            + "/../test_resources/CLK/V3/demo.txt";
+        let rinex = Rinex::from_file(&test_resource);
+        assert_eq!(rinex.is_ok(), true);
+        let rinex = rinex.unwrap();
+        assert_eq!(rinex.is_clocks_rinex(), true);
+    }
+
+    #[test]
+    fn is_new_epoch() {
+        let l = "AR AREQ 1994 07 14 20 59  0.000000  6   -0.123456789012E+00 -0.123456789012E+01"; 
+        assert_eq!(clocks::is_new_epoch(l), true);
+        let l = "RA AREQ 1994 07 14 20 59  0.000000  6   -0.123456789012E+00 -0.123456789012E+01"; 
+        assert_eq!(clocks::is_new_epoch(l), false);
+        let l = "DR AREQ 1994 07 14 20 59  0.000000  6   -0.123456789012E+00 -0.123456789012E+01"; 
+        assert_eq!(clocks::is_new_epoch(l), true);
+        let l = "CR AREQ 1994 07 14 20 59  0.000000  6   -0.123456789012E+00 -0.123456789012E+01"; 
+        assert_eq!(clocks::is_new_epoch(l), true);
+        let l = "AS AREQ 1994 07 14 20 59  0.000000  6   -0.123456789012E+00 -0.123456789012E+01"; 
+        assert_eq!(clocks::is_new_epoch(l), true);
+    }
+}
--- rinex/tests/obs.rs
+++ rinex/tests/obs.rs
@@ -15,4 +15,70 @@ mod test {
         assert_eq!(rinex.header.obs.is_some(), true);
         assert_eq!(rinex.header.meteo.is_none(), true);
     }
+    #[test]
+    fn test_is_new_epoch() {
+        assert_eq!(        
+            observation::is_new_epoch("95 01 01 00 00 00.0000000  0  7 06 17 21 22 23 28 31",
+                version::Version {
+                    major: 2,
+                    minor: 0,
+                }
+            ),
+            true
+        );
+        assert_eq!(        
+            observation::is_new_epoch("21700656.31447  16909599.97044          .00041  24479973.67844  24479975.23247",
+                version::Version {
+                    major: 2,
+                    minor: 0,
+                }
+            ),
+            false
+        );
+        assert_eq!(        
+            observation::is_new_epoch("95 01 01 11 00 00.0000000  0  8 04 16 18 19 22 24 27 29",
+                version::Version {
+                    major: 2,
+                    minor: 0,
+                }
+            ),
+            true
+        );
+        assert_eq!(        
+            observation::is_new_epoch("95 01 01 11 00 00.0000000  0  8 04 16 18 19 22 24 27 29",
+                version::Version {
+                    major: 3,
+                    minor: 0,
+                }
+            ),
+            false 
+        );
+        assert_eq!(        
+            observation::is_new_epoch("> 2022 01 09 00 00 30.0000000  0 40",
+                version::Version {
+                    major: 3,
+                    minor: 0,
+                }
+            ),
+            true 
+        );
+        assert_eq!(        
+            observation::is_new_epoch("> 2022 01 09 00 00 30.0000000  0 40",
+                version::Version {
+                    major: 2,
+                    minor: 0,
+                }
+            ),
+            false
+        );
+        assert_eq!(        
+            observation::is_new_epoch("G01  22331467.880   117352685.28208        48.950    22331469.28",
+                version::Version {
+                    major: 3,
+                    minor: 0,
+                }
+            ),
+            false
+        );
+    }
 }
