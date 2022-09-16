bb4dc862c42679bde5a44b53ecc4b28668f5673b
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2022-09-16 22:10:16 +0200

    can't use diff as file production test tool, due to header unpredictable header fields order of appearance

--- rinex/tests/production.rs
+++ rinex/tests/production.rs
@@ -2,20 +2,6 @@
 mod test {
     use rinex::*;
     //use std::str::FromStr;
-    use std::process::Command;
-    /// Runs `diff` to determines whether f1 & f2 
-    /// are strictly identical or not
-    fn diff_is_strictly_identical (f1: &str, f2: &str) -> Result<bool, std::string::FromUtf8Error> {
-        let output = Command::new("diff")
-            .arg("-q")
-            .arg("-Z")
-            .arg(f1)
-            .arg(f2)
-            .output()
-            .expect("failed to execute \"diff\"");
-        let output = String::from_utf8(output.stdout)?;
-        Ok(output.len()==0)
-    }
     #[test]
     fn test_obs_v2_production() {
         let folder = env!("CARGO_MANIFEST_DIR").to_owned() + "/../test_resources/OBS/V2/";
@@ -30,7 +16,7 @@ mod test {
             // produce a copy
             let copy_path = fp.to_owned() + "-copy";
             assert_eq!(rinex.to_file(&copy_path).is_ok(), true);
-            // remove copy to not disturb other tests browser
+            // remove copy not to disturb other tests browser
             let _ = std::fs::remove_file(copy_path);
         }
     }
@@ -48,7 +34,7 @@ mod test {
             // produce a copy
             let copy_path = fp.to_owned() + "-copy";
             assert_eq!(rinex.to_file(&copy_path).is_ok(), true);
-            // remove copy to not disturb other tests browser
+            // remove copy not to disturb other tests browser
             let _ = std::fs::remove_file(copy_path);
         }
     }
@@ -66,7 +52,25 @@ mod test {
             // produce a copy
             let copy_path = fp.to_owned() + "-copy";
             assert_eq!(rinex.to_file(&copy_path).is_ok(), true);
-            // remove copy to not disturb other tests browser
+            // remove copy not to disturb other tests browser
+            let _ = std::fs::remove_file(copy_path);
+        }
+    }
+    #[test]
+    fn test_meteo_v4_production() {
+        let folder = env!("CARGO_MANIFEST_DIR").to_owned() + "/../test_resources/MET/V4/";
+        for file in std::fs::read_dir(folder).unwrap() {
+            let fp = file.unwrap();
+            let fp = fp.path();
+            let fp = fp.to_str().unwrap();
+            // parse this rinex
+            let rinex = Rinex::from_file(fp);
+            assert_eq!(rinex.is_ok(), true);
+            let rinex = rinex.unwrap();
+            // produce a copy
+            let copy_path = fp.to_owned() + "-copy";
+            assert_eq!(rinex.to_file(&copy_path).is_ok(), true);
+            // remove copy not to disturb other tests browser
             let _ = std::fs::remove_file(copy_path);
         }
     }
@@ -84,7 +88,7 @@ mod test {
             // produce a copy
             let copy_path = fp.to_owned() + "-copy";
             assert_eq!(rinex.to_file(&copy_path).is_ok(), true);
-            // remove copy to not disturb other tests browser
+            // remove copy not to disturb other tests browser
             let _ = std::fs::remove_file(copy_path);
         }
     }
