a20be52cbea923264b8c20fe526e4fa34fffe648
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2022-09-26 08:28:02 +0200

    unlock some new data generation

--- rinex/tests/production.rs
+++ rinex/tests/production.rs
@@ -3,7 +3,7 @@ mod test {
     use rinex::*;
     //use std::str::FromStr;
     #[test]
-    fn test_obs_v2_production() {
+    fn obs_v2_production() {
         let folder = env!("CARGO_MANIFEST_DIR").to_owned() + "/../test_resources/OBS/V2/";
         for file in std::fs::read_dir(folder).unwrap() {
             let fp = file.unwrap();
@@ -16,12 +16,12 @@ mod test {
             // produce a copy
             let copy_path = fp.to_owned() + "-copy";
             assert_eq!(rinex.to_file(&copy_path).is_ok(), true);
-            // remove copy not to disturb other tests browser
+            // remove copy not to disturb other test browsers
             let _ = std::fs::remove_file(copy_path);
         }
     }
     #[test]
-    fn test_obs_v3_production() {
+    fn obs_v3_production() {
         let folder = env!("CARGO_MANIFEST_DIR").to_owned() + "/../test_resources/OBS/V3/";
         for file in std::fs::read_dir(folder).unwrap() {
             let fp = file.unwrap();
@@ -34,12 +34,12 @@ mod test {
             // produce a copy
             let copy_path = fp.to_owned() + "-copy";
             assert_eq!(rinex.to_file(&copy_path).is_ok(), true);
-            // remove copy not to disturb other tests browser
+            // remove copy not to disturb other test browsers
             let _ = std::fs::remove_file(copy_path);
         }
     }
     #[test]
-    fn test_meteo_v2_production() {
+    fn meteo_v2_production() {
         let folder = env!("CARGO_MANIFEST_DIR").to_owned() + "/../test_resources/MET/V2/";
         for file in std::fs::read_dir(folder).unwrap() {
             let fp = file.unwrap();
@@ -52,12 +52,12 @@ mod test {
             // produce a copy
             let copy_path = fp.to_owned() + "-copy";
             assert_eq!(rinex.to_file(&copy_path).is_ok(), true);
-            // remove copy not to disturb other tests browser
+            // remove copy not to disturb other test browsers
             let _ = std::fs::remove_file(copy_path);
         }
     }
     #[test]
-    fn test_meteo_v4_production() {
+    fn meteo_v4_production() {
         let folder = env!("CARGO_MANIFEST_DIR").to_owned() + "/../test_resources/MET/V4/";
         for file in std::fs::read_dir(folder).unwrap() {
             let fp = file.unwrap();
@@ -70,12 +70,12 @@ mod test {
             // produce a copy
             let copy_path = fp.to_owned() + "-copy";
             assert_eq!(rinex.to_file(&copy_path).is_ok(), true);
-            // remove copy not to disturb other tests browser
+            // remove copy not to disturb other test browsers
             let _ = std::fs::remove_file(copy_path);
         }
     }
     #[test]
-    fn test_nav_v2_production() {
+    fn nav_v2_production() {
         let folder = env!("CARGO_MANIFEST_DIR").to_owned() + "/../test_resources/NAV/V2/";
         for file in std::fs::read_dir(folder).unwrap() {
             let fp = file.unwrap();
@@ -88,7 +88,43 @@ mod test {
             // produce a copy
             let copy_path = fp.to_owned() + "-copy";
             assert_eq!(rinex.to_file(&copy_path).is_ok(), true);
-            // remove copy not to disturb other tests browser
+            // remove copy not to disturb other test browsers
+            let _ = std::fs::remove_file(copy_path);
+        }
+    }
+    #[test]
+    fn nav_v3_production() {
+        let folder = env!("CARGO_MANIFEST_DIR").to_owned() + "/../test_resources/NAV/V3/";
+        for file in std::fs::read_dir(folder).unwrap() {
+            let fp = file.unwrap();
+            let fp = fp.path();
+            let fp = fp.to_str().unwrap();
+            // parse this rinex
+            let rinex = Rinex::from_file(fp);
+            assert_eq!(rinex.is_ok(), true);
+            let rinex = rinex.unwrap();
+            // produce a copy
+            let copy_path = fp.to_owned() + "-copy";
+            assert_eq!(rinex.to_file(&copy_path).is_ok(), true);
+            // remove copy not to disturb other test browsers
+            let _ = std::fs::remove_file(copy_path);
+        }
+    }
+    #[test]
+    fn nav_v4_production() {
+        let folder = env!("CARGO_MANIFEST_DIR").to_owned() + "/../test_resources/NAV/V4/";
+        for file in std::fs::read_dir(folder).unwrap() {
+            let fp = file.unwrap();
+            let fp = fp.path();
+            let fp = fp.to_str().unwrap();
+            // parse this rinex
+            let rinex = Rinex::from_file(fp);
+            assert_eq!(rinex.is_ok(), true);
+            let rinex = rinex.unwrap();
+            // produce a copy
+            let copy_path = fp.to_owned() + "-copy";
+            assert_eq!(rinex.to_file(&copy_path).is_ok(), true);
+            // remove copy not to disturb other test browsers
             let _ = std::fs::remove_file(copy_path);
         }
     }
