af704a13578a5245b89d8f290ef97e9c10b49d60
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2022-10-17 20:01:54 +0200

    return read only reference, save a malloc()

--- rinex/src/hatanaka/textdiff.rs
+++ rinex/src/hatanaka/textdiff.rs
@@ -1,6 +1,6 @@
 #[derive(Debug)]
 pub struct TextDiff {
-    pub buffer: String,
+    pub buffer: String, 
 }
 
 impl TextDiff {
@@ -14,7 +14,7 @@ impl TextDiff {
     
     /// Initializes `Text` differentiator
     pub fn init (&mut self, data: &str) {
-        self.buffer = data.to_string() ;
+        self.buffer = data.to_string(); 
     }
 
     // Returns positions of bytes that differ, in s1 as compared
@@ -45,7 +45,7 @@ impl TextDiff {
     }
 
     /// Decompresses given data
-    pub fn decompress (&mut self, data: &str) -> String {
+    pub fn decompress (&mut self, data: &str) -> &str {
         let diffs = Self::diff(&self.buffer, data);
 
         for pos in diffs {
@@ -60,77 +60,18 @@ impl TextDiff {
         }
         
         // manage special whitespace insertions
+        //self.buffer = &self.buffer.replace("&", " ");
         while let Some(pos) = self.buffer.as_str().find("&") {
             self.buffer
                 .replace_range(pos..pos+1, " ");
         }
-
-        //previous logic always ommits last byte in data
-        /*let last_byte_pos = data.len()-1;
-        let last_byte =  &data[last_byte_pos-1..last_byte_pos];
-        if last_byte != " " {
-            // ==> overwrite last byte
-            self.buffer
-                .replace_range(last_byte_pos-1..last_byte_pos, last_byte);
-        }*/
-
-        // when decompressing, we expose internal content as is
-/*
-        let max = std::cmp::max(internal.len(), new.len());
-        for i in 0..max {
-            if new[i] != ' ' {
-                // not a whitespace
-                //  means overwrite internal content
-                if new[i] == '&' {
-                    // \& means whitespace insertion
-                    internal[i] = '&';
-                } else {
-                    internal[i] = new[i];
-                }
-            }
-        }
-*/
-/*
-        let l = self.buffer.len();
-        let mut p = self.buffer
-            .as_mut_str()
-            .chars();
-        let mut data = data.chars();
-        for _ in 0..l {
-            let next_c = p.next().unwrap();
-            if let Some(c) = data.next() {
-                if c == '&' { // special whitespace insertion
-                    recovered.push_str(" ")
-                } else if c.is_ascii_alphanumeric() {
-                    recovered.push_str(&c.to_string())
-                } else {
-                    recovered.push_str(&next_c.to_string())
-                }
-            } else {
-                recovered.push_str(&next_c.to_string())
-            }
-        }
-        // mask might be longer than self
-        // in case we need to extend current value
-        loop {
-            if let Some(c) = data.next() {
-                if c == '&' { // special whitespace insertion
-                    recovered.push_str(" ")
-                } else if c.is_ascii_alphanumeric() {
-                    recovered.push_str(&c.to_string())
-                }
-            } else {
-                break
-            }
-        }
-        self.buffer = recovered.clone(); // for next time
-        recovered */
-        self.buffer.clone()
+        
+        &self.buffer
     }
     
     /// Compresses given data
     pub fn compress (&mut self, data: &str) -> String {
-        let mut result = String::new();
+        /*let mut result = String::new();
         let inner: Vec<_> = self.buffer.chars().collect();
         self.buffer.clear();
         let to_compress: Vec<_> = data.chars().collect();
@@ -158,7 +99,8 @@ impl TextDiff {
             }
         }
 
-        result.to_string()
+        result.to_string()*/
+        String::from("hello")
     }
 }
 
