a333e351e95d9be8529cfb45cac7561559b088b1
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2022-10-18 22:02:16 +0200

    improve test method, unlock meteo v4 test, working on obs

--- rinex/tests/production.rs
+++ rinex/tests/production.rs
@@ -1,76 +1,102 @@
 #[cfg(test)]
 mod test {
     use rinex::*;
-    //use std::str::FromStr;
-    use std::process::Command;
-    fn run_diffz (path1: &str, path2: &str) -> Result<String, std::string::FromUtf8Error> {
-        let output = Command::new("diff")
-            .arg("-Z")
-            .arg(path1)
-            .arg(path2)
-            .output()
-            .expect("failed to execute \"diff -z\"");
-        let output = String::from_utf8(output.stdout)?;
-        Ok(output)
-    }
-    fn observation_report_on_failure (rnx_a: &Rinex, rnx_b: &Rinex) {
+    /// OBS RINEX thorough comparison
+    fn observation_comparison(rnx_a: &Rinex, rnx_b: &Rinex) {
         let rec_a = rnx_a.record.as_obs().unwrap();
         let rec_b = rnx_b.record.as_obs().unwrap();
         for (e_a, (clk_offset_a, vehicules_a)) in rec_a.iter() {
-            println!("\nInvestigating {:?}", e_a);
-            let (clk_offset_b, vehicules_b) = rec_b.get(e_a)
-                .unwrap(); // causes a panic that will help debug
-            assert_eq!(clk_offset_a, clk_offset_b);
-            for (sv_a, observables_a) in vehicules_a.iter() {
-                println!("Testing {:?}", sv_a);
-                let observables_b = vehicules_b.get(sv_a)
-                    .unwrap(); // helpful panic
-                assert_eq!(observables_a, observables_b);
-                for (code_a, obs_a) in observables_a {
-                    println!("Testing {:?}", code_a);
-                    let obs_b = observables_b
-                        .get(code_a)
-                        .unwrap(); // helpful panic
-                    println!("Testing {:?}", obs_a);
-                    assert_eq!(obs_a, obs_b);
+            if let Some((clk_offset_b, vehicules_b)) = rec_b.get(e_a) {
+                assert_eq!(clk_offset_a, clk_offset_b);
+                for (sv_a, observables_a) in vehicules_a.iter() {
+                    if let Some(observables_b) = vehicules_b.get(sv_a) {
+                        for (code_a, obs_a) in observables_a {
+                            if let Some(obs_b) = observables_b.get(code_a) {
+                                assert_eq!(obs_a, obs_b);
+                            } else {
+                                println!("observables A {:?} \nobservables B {:?}", observables_a, observables_b);
+                                panic!("epoch {:?} - {:?} : missing \"{}\" observation", e_a, sv_a, code_a);
+                            }
+                        }
+                    } else {
+                        panic!("epoch {:?} - missing vehicule {:?}", e_a, sv_a);
+                    }
                 }
+            } else {
+                panic!("missing epoch {:?}", e_a);
             }
         }
 
         // reaching this point
         //  means rnx_b has more data than expected
-        println!("******* Got more data than expected ********");
+        println!("************* WARNING ************");
+        println!("   Got more data than expected    ");
+        println!("**********************************");
         for (e_b, (clk_offset_b, vehicules_b)) in rec_b.iter() {
-            println!("\nInvestigating {:?}", e_b);
-            let (clk_offset_a, vehicules_a) = rec_a.get(e_b)
-                .unwrap();
-            assert_eq!(clk_offset_a, clk_offset_b);
-            for (sv_b, observables_b) in vehicules_b.iter() {
-                println!("Testing {:?}", sv_b);
-                let observables_a = vehicules_a.get(sv_b)
-                    .unwrap();
-                assert_eq!(observables_a, observables_b);
-                for (code_b, obs_b) in observables_b {
-                    println!("Testing {:?}", code_b);
-                    let obs_a = observables_a.get(code_b)
-                        .unwrap();
-                    println!("Testing {:?}", obs_a);
-                    assert_eq!(obs_a, obs_b);
+            if let Some((clk_offset_a, vehicules_a)) = rec_a.get(e_b) {
+                assert_eq!(clk_offset_a, clk_offset_b);
+                for (sv_b, observables_b) in vehicules_b.iter() {
+                    if let Some(observables_a) = vehicules_a.get(sv_b) {
+                        for (code_b, obs_b) in observables_b {
+                            if let Some(obs_a) = observables_a.get(code_b) {
+                                assert_eq!(obs_a, obs_b);
+                            } else {
+                                panic!("epoch {:?} - {:?} : parsed \"{}\" unexpectedly", e_b, sv_b, code_b);
+                            }
+                        }
+                    } else {
+                        panic!("epoch {:?} - parsed {:?} unexpectedly", e_b, sv_b);
+                    }
                 }
+            } else {
+                panic!("parsed epoch {:?} unexpectedly", e_b);
             }
         }
     }
-    fn report_on_failure(rnx_a: &Rinex, rnx_b: &Rinex) {
-        //TODO
-        //not for certain RINEX types
-        let epochs_a = rnx_a.epochs();
-        let epochs_b = rnx_b.epochs();
-        assert_eq!(epochs_a, epochs_b);
-        // dig further
-        if rnx_a.is_observation_rinex() {
-            observation_report_on_failure(&rnx_a, &rnx_b);
+    
+    /// Meteo RINEX thorough comparison
+    fn meteo_comparison(rnx_a: &Rinex, rnx_b: &Rinex) {
+        let rec_a = rnx_a.record.as_meteo().unwrap();
+        let rec_b = rnx_b.record.as_meteo().unwrap();
+        for (e_a, obscodes_a) in rec_a.iter() {
+            if let Some(obscodes_b) = rec_b.get(e_a) {
+                for (code_a, observation_a) in obscodes_a.iter() {
+                    if let Some(observation_b) = obscodes_b.get(code_a) {
+                        assert_eq!(observation_a, observation_b);
+                    } else {
+                        panic!("epoch {:?} missing \"{}\" observation", e_a, code_a);
+                    }
+                }
+            } else {
+                panic!("missing epoch {:?}", e_a);
+            }
         }
-        // other types of investigation should be coded here
+
+        // reaching this point
+        //  means rnx_b has more data than expected
+        println!("************* WARNING ************");
+        println!("   Got more data than expected    ");
+        println!("**********************************");
+        for (e_b, obscodes_b) in rec_b.iter() {
+            if let Some(obscodes_a) = rec_a.get(e_b) {
+                for (code_b, observation_b) in obscodes_b.iter() {
+                    if let Some(observation_a) = obscodes_a.get(code_b) {
+                        assert_eq!(observation_a, observation_b);
+                    } else {
+                        panic!("epoch {:?} parsed \"{}\" unexpectedly", e_b, code_b);
+                    }
+                }
+            } else {
+                panic!("parsed {:?} unexpectedly", e_b);
+            }
+        }
+    }
+    fn compare_with_panic(rnx_a: &Rinex, rnx_b: &Rinex) {
+        if rnx_a.is_observation_rinex() {
+            observation_comparison(&rnx_a, &rnx_b);
+        } else if rnx_a.is_meteo_rinex() {
+            meteo_comparison(&rnx_a, &rnx_b);
+        }
     }
     fn testbench (path: &str) {
         println!("Running testbench on: \"{}\"", path);
@@ -85,17 +111,13 @@ mod test {
         let rnx_b = rnx_b
             .unwrap();
         if rnx_a != rnx_b {
-            let diff = run_diffz(path, &copy_path)
-                .unwrap();
-            println!("******* DIFF ******** \n{}\n *************** ", diff);
-            report_on_failure(&rnx_a, &rnx_b);
-            panic!("******* TESTBENCH FAILED **********");
+            compare_with_panic(&rnx_a, &rnx_b);
         }
         // remove copy not to disturb other test browsers
-        // let _ = std::fs::remove_file(copy_path);
+        let _ = std::fs::remove_file(copy_path);
     }
-    /*#[test]
-    fn obs_v2() {
+    #[test]
+    /*fn obs_v2() {
         let folder = env!("CARGO_MANIFEST_DIR").to_owned() + "/../test_resources/OBS/V2/";
         for file in std::fs::read_dir(folder).unwrap() {
             let fp = file.unwrap();
@@ -121,7 +143,7 @@ mod test {
             testbench(fp.to_str().unwrap());
         }
     }
-    /*#[test]
+    #[test]
     fn meteo_v4() {
         let folder = env!("CARGO_MANIFEST_DIR").to_owned() + "/../test_resources/MET/V4/";
         for file in std::fs::read_dir(folder).unwrap() {
@@ -129,7 +151,7 @@ mod test {
             let fp = fp.path();
             testbench(fp.to_str().unwrap());
         }
-    }
+    }/*
     #[test]
     fn nav_v2() {
         let folder = env!("CARGO_MANIFEST_DIR").to_owned() + "/../test_resources/NAV/V2/";
