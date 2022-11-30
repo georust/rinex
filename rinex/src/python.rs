7c597279708d2b01b3961d5f4c10e20e40f756a1
Lars Næsbye Christensen <lars@naesbye.dk>
2022-11-30 22:10:04 +0100

    remove unused and outcommented

--- rinex/src/python.rs
+++ rinex/src/python.rs
@@ -2,12 +2,6 @@ use pyo3::{exceptions::PyException, prelude::*};
 
 use crate::prelude::*;
 
-/*impl std::convert::From<Error> for PyErr {
-    fn from(err: Error) -> PyErr {
-        PyException::new_err(err.to_string())
-    }
-}*/
-
 #[pymodule]
 fn rinex(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<EpochFlag>()?;
