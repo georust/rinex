1ef66be24361c4f147a6d3eed3dcd11e50aa6fb7
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2022-12-01 11:31:17 +0100

    add pre release script

--- .github/workflows/pre-release.sh
+++ .github/workflows/pre-release.sh
@@ -1,4 +1,2 @@
 rm -rf /github/workspace/output/rinex-cli
-ls /usr/lib -lah
-ls /usr/lib/x86-64_linux-gnu -lah
-export PKG_CONFIG_PATH=/usr/lib/x86-64_linux-gnu/pkgconfig/fontconfig.pc
+export PKG_CONFIG_PATH=/usr/lib/pkgconfig/fontconfig.pc
