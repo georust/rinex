522eadfd78008a2ba66b8bd265edcd456d3830e9
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2022-12-08 10:55:42 +0100

    coverage: add more tests
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- rinex/src/antex/frequency.rs
+++ rinex/src/antex/frequency.rs
@@ -1,5 +1,5 @@
 //! Antex - special RINEX type specific structures
-use crate::channel;
+use crate::channel::Channel;
 
 #[derive(Debug, Clone, PartialEq, PartialOrd)]
 #[cfg_attr(feature = "serde", derive(Serialize))]
@@ -48,7 +48,7 @@ impl Pattern {
 #[cfg_attr(feature = "serde", derive(Serialize))]
 pub struct Frequency {
     /// Channel, example: L1, L2 for GPS, E1, E5 for GAL...
-    pub channel: channel::Channel,
+    pub channel: Channel,
     /// Northern component of the mean antenna phase center
     /// relative to the antenna reference point (ARP),
     /// in [mm]
@@ -70,7 +70,7 @@ pub struct Frequency {
 impl Default for Frequency {
     fn default() -> Self {
         Self {
-            channel: channel::Channel::default(),
+            channel: Channel::default(),
             north: 0.0_f64,
             east: 0.0_f64,
             up: 0.0_f64,
@@ -80,7 +80,53 @@ impl Default for Frequency {
 }
 
 impl Frequency {
-    pub fn with_channel(&self, channel: channel::Channel) -> Self {
+    /// Returns ARP coordinates in Geodetic system.
+    /// Ref. position must be given in (lat, lon, altitude) [m]
+    pub fn arp_geodetic(&self, ref_pos: (f64, f64, f64)) -> (f64, f64, f64) {
+        map_3d::enu2geodetic(
+            self.east * 10.0E-3,
+            self.north * 10.0E-3,
+            self.up * 10.0E-3,
+            ref_pos.0,
+            ref_pos.1,
+            ref_pos.2,
+            map_3d::Ellipsoid::WGS84,
+        )
+    }
+    /// Returns ARP coordinates in ECEF system.
+    pub fn arp_ecef(&self, ref_pos: (f64, f64, f64)) -> (f64, f64, f64) {
+        map_3d::enu2ecef(
+            self.east * 10.0E-3,
+            self.north * 10.0E-3,
+            self.up * 10.0E-3,
+            ref_pos.0,
+            ref_pos.1,
+            ref_pos.2,
+            map_3d::Ellipsoid::WGS84,
+        )
+    }
+    /// Returns ARP coordinates as North Earth Down coordinates
+    pub fn arp_ned(&self, ref_pos: (f64, f64, f64)) -> (f64, f64, f64) {
+        let ecef = map_3d::enu2ecef(
+            self.east * 10.0E-3,
+            self.north * 10.0E-3,
+            self.up * 10.0E-3,
+            ref_pos.0,
+            ref_pos.1,
+            ref_pos.2,
+            map_3d::Ellipsoid::WGS84,
+        );
+        map_3d::ecef2ned(
+            ecef.0,
+            ecef.1,
+            ecef.2,
+            ref_pos.0,
+            ref_pos.1,
+            ref_pos.2,
+            map_3d::Ellipsoid::WGS84,
+        )
+    }
+    pub fn with_channel(&self, channel: Channel) -> Self {
         let mut f = self.clone();
         f.channel = channel.clone();
         f
@@ -104,5 +150,24 @@ impl Frequency {
         let mut f = self.clone();
         f.patterns.push(p.clone());
         f
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test_pattern() {
+        let default = Pattern::default();
+        assert_eq!(default, Pattern::NonAzimuthDependent(Vec::new()));
+        assert_eq!(default.is_azimuth_dependent(), false);
+    }
+    #[test]
+    fn test_frequency() {
+        let default = Frequency::default();
+        assert_eq!(default.channel, Channel::default());
+        assert_eq!(default.north, 0.0_f64);
+        assert_eq!(default.east, 0.0_f64);
+        assert_eq!(default.up, 0.0_f64);
     }
 }
