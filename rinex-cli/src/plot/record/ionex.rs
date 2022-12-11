159b1135049532a0ab22cd1b389f9ca206ecdc9e
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2022-12-11 18:34:11 +0100

    cli: working on ionex map plot
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- rinex-cli/src/plot/record/ionex.rs
+++ rinex-cli/src/plot/record/ionex.rs
@@ -1,50 +1,80 @@
 use crate::plot::Context;
+//use itertools::Itertools;
 use plotly::{
     color::NamedColor,
-    common::{Marker, MarkerSymbol},
-    layout::{Center, DragMode, Mapbox, MapboxStyle, Margin},
-    ImageFormat, Layout, Plot, Scatter, ScatterMapbox,
+    common::{Marker, MarkerSymbol,}, //color::Rgba},
+    layout::MapboxStyle,
+    //scatter_mapbox::Fill,
+    ScatterMapbox,
 };
-use rinex::{ionex::*, prelude::*};
+use rinex::{ionex::*};
 
-pub fn plot_tec_map(ctx: &mut Context, borders: ((f32, f32), (f32, f32)), record: &Record) {
-    let map_center = (
-        (borders.1 .0 - borders.0 .0) / 2.0,
-        (borders.1 .1 - borders.0 .1) / 2.0,
-    );
+pub fn plot_tec_map(ctx: &mut Context, borders: ((f64, f64), (f64, f64)), record: &Record) {
+    let cmap = colorous::TURBO;
+    ctx.add_world_map(MapboxStyle::OpenStreetMap, (32.5, -40.0), 1);
 
-    let mut latitudes: Vec<f64> = Vec::new();
-    let mut longitudes: Vec<f64> = Vec::new();
-    for (e, (tec, _, _)) in record {
+    let mut grid_lat: Vec<f64> = Vec::new();
+    let mut grid_lon: Vec<f64> = Vec::new();
+    let mut tec_max = -std::f64::INFINITY;
+    for (e_index, (e, (tec, _, _))) in record.iter().enumerate() {
         for point in tec {
-            latitudes.push(point.latitude.into());
-            longitudes.push(point.longitude.into());
+            if e_index == 0 { // grab grid definition
+                grid_lat.push(point.latitude.into());
+                grid_lon.push(point.longitude.into());
+            }
+            if point.value > tec_max {
+                tec_max = point.value;
+            }
         }
-        break; // only care about 1 epoch for this
     }
 
-    let grid = ScatterMapbox::new(latitudes, longitudes).marker(
-        Marker::new()
-            .size(5)
-            .symbol(MarkerSymbol::Circle)
-            .color(NamedColor::Black)
-            .opacity(0.2),
-    );
+    let grid = ScatterMapbox::new(grid_lat, grid_lon)
+        .marker(
+            Marker::new()
+                .size(5)
+                .symbol(MarkerSymbol::Circle)
+                .color(NamedColor::Black)
+                .opacity(0.5)
+        )
+        .name("TEC Grid");
+    ctx.add_trace(grid);
 
-    let layout = Layout::new()
-        .drag_mode(DragMode::Zoom)
-        .margin(Margin::new().top(0).left(0).bottom(0).right(0))
-        .mapbox(
-            Mapbox::new()
-                .style(MapboxStyle::OpenStreetMap)
-                //.center(Center::new(45.5017, -73.5673))
-                .center(Center::new(32.5, -40.0))
-                .zoom(1),
-        );
-
-    let mut plot = Plot::new();
-    plot.add_trace(grid);
-    plot.set_layout(layout);
-
-    plot.show();
+    /*
+     * Build heat map,
+     * we have no means to plot several heat map (day course)
+     * at the moment, we just plot the 1st epoch
+     */
+     /*
+    for (e_index, (e, (tec, _, _))) in record.iter().enumerate() {
+        if e_index == 0 {
+            // form the smallest area from that grid
+            for rows in 
+            for i in 0..tec.len() / 4 {
+                println!("MAPS {}", i);
+                //for i in 0..maps.len() / 2 {
+                /*
+                let mut row1 = maps[0]
+                let mut row2 = maps[1]
+                if let Some(points12) = row1.next() {
+                    if let Some(points34) = row2.next() {
+                        // average TEC in that area
+                        let tec = (points12[0].value + points12[1].value
+                            +points34[0].value + points34[1].value ) / 4.0;
+                        let color = cmap.eval_continuous(tec / tec_max);
+                        let area = ScatterMapbox::new(
+                            vec![points12[0].latitude, points12[1].latitude, points34[0].latitude, points34[1].latitude],
+                            vec![points12[0].longitude, points12[1].longitude, points34[0].longitude, points34[1].longitude],
+                        )
+                        .opacity(0.5)
+                        .fill(Fill::ToSelf)
+                        .fill_color(Rgba::new(color.r, color.g, color.b, 0.9));
+                        ctx.add_trace(area);
+                        println!("YES");
+                    }
+                }*/
+            }
+        } else {
+            break ; // we need animations for that
+        }
+    }*/
 }
