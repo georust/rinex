9b309c8f4da27e6bd6f88ffd330c17d44e772eab
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2023-07-30 15:49:32 +0200

    introduce coordinates macros
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- rinex/src/macros.rs
+++ rinex/src/macros.rs
@@ -1,23 +1,26 @@
-//! Macros and helpers 
+//! Macros and helpers
 
-/// Creates an [sv::Sv] from given string description,
+/// Creates an [crate::Sv] from given string description,
 /// which must be valid.
+#[macro_export]
 macro_rules! sv {
     ($desc: expr) => {
         Sv::from_str($desc).unwrap()
     };
 }
 
-/// Creates a [constellation::Constellation] from given string
+/// Creates a [crate::Constellation] from given string
 /// description, which must be valid.
+#[macro_export]
 macro_rules! gnss {
     ($desc: expr) => {
         Constellation::from_str($desc).unwrap()
     };
 }
 
-/// Creates an [observable::Observable] from given string
+/// Creates an [crate::Observable] from given string
 /// description, which must be valid.
+#[macro_export]
 macro_rules! observable {
     ($desc: expr) => {
         Observable::from_str($desc).unwrap()
@@ -25,8 +28,33 @@ macro_rules! observable {
 }
 
 /// Returns a filter object, from a given description which must be valid
+#[macro_export]
 macro_rules! filter {
     ($desc: expr) => {
         Filter::from_str($desc).unwrap()
+    };
+}
+
+/// Returns `true` if given `Rinex` line is a comment
+#[macro_export]
+macro_rules! is_comment {
+    ($line: expr) => {
+        $line.trim_end().ends_with("COMMENT")
+    };
+}
+
+/// Builds a [crate::GroundPosition] in WGS84
+#[macro_export]
+macro_rules! wgs84 {
+    ($desc: expr) => {
+        GroundPosition::from_ecef_wgs84(($desc.0, $desc.1, $desc.2))
+    };
+}
+
+/// Builds a [crate::GroundPosition] from geodetic coordinates in ddeg
+#[macro_export]
+macro_rules! geodetic {
+    ($desc: expr) => {
+        GroundPosition::from_geodetic(($desc.0, $desc.1, $desc.2))
     };
 }
