b5801931bd493643f50f922aae3e0d2f17cd5907
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2023-08-06 16:12:46 +0200

    tools: add sv ephemeris ci helper
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- tools/sv_elev_azim.py
+++ tools/sv_elev_azim.py
@@ -9,10 +9,12 @@
 import os
 import sys
 import math
+import xarray
 import georinex as gr
+from datetime import datetime, timedelta
 
 gr_kepler_fields = [
-    "GPSWeek",
+    "Week",
     "Toe",
     "Eccentricity",
     "sqrtA",
@@ -41,9 +43,45 @@ def kepler_hasnan(kepler):
             return True
     return False
 
+def kepler_has_weekcounter(kepler):
+    for wk in ["GPSWeek", "GALWeek", "BDTWeek"]:
+        if wk in kepler:
+            return True
+    return False
+
+def kepler_ready(kepler):
+    if kepler_hasnan(kepler):
+        return False
+    if not(kepler_has_weekcounter(kepler)):
+        return False
+    for key in gr_kepler_fields:
+        if key != "Week": # week counter..
+            if not(key in kepler):
+                return False # key is missing
+    return True
+
+def sv_is_glonass(sv):
+    return sv[0] == 'R'
+
+def sv_to_constell(sv):
+    if sv[0] == 'G':
+        return "GPS"
+    elif sv[0] == 'E':
+        return "GAL"
+    elif sv[0] == 'C':
+        return "BDT"
+    elif sv[0] == 'J':
+        return "GPS"
+    else:
+        return None
+
+def constell_t0(sv):
+    #if sv[0] == 'G':
+    return datetime(1980, 1, 6)
+
 def main(argv):
     if len(argv) == 0:
-        print("./sv_elev_azim.py [test_resources/]")
+        print("[test_pool_dir]")
         return 0
     
     base_dir = argv[0]
@@ -60,6 +98,8 @@ def main(argv):
             for key in known_ref_positions.keys():
                 if key in nav_path:
                     known_ref_pos = known_ref_positions[key]
+            if known_ref_pos is None:
+                known_ref_pos = (-5.67841101e6, -2.49239629e7, 7.05651887e6)
 
             txt_path = base_dir + "/gr/{}/{}.txt".format(rev, fp)
             with open(txt_path, "w") as fd:
@@ -68,15 +108,43 @@ def main(argv):
                 for epoch in epochs :
                     data = nav.sel(time=epoch)
                     for sv in vehicles:
+                        if sv_is_glonass(sv):
+                            continue # GLO: NOT YET
+                        if sv_to_constell(sv) is None:
+                            continue # GNSS: not supported yet or unknown definition
+
                         sv_data = data.sel(sv=sv)
                         kepler = {}
                         for field in gr_kepler_fields:
+                            if field == "Week":
+                                # week counter special case
+                                field = sv_to_constell(sv) + field 
                             value = sv_data.variables[field].values
                             kepler[field] = value
                         if not kepler_hasnan(kepler):
                             # kepler struct fully defined, can determine (Elev°,Azim°)
-                            # print(epoch, sv, kepler)
-                            content = "epoch, {}, ref_pos, {}, sv, {}, ".format(epoch, str(known_ref_pos), sv)
+                            # print(epoch, sv, "READY: ", kepler_ready(kepler), kepler)
+                            
+                            tgnss = constell_t0(sv)
+                            #tgnss += timedelta(weeks=kepler[sv_to_constell(sv) + "Week"])
+                            #tgnss+= epoch - previous_sunday.midnight in seconds
+
+                            (xref, yref, zref) = known_ref_pos
+                            struct = xarray.Dataset(
+                                kepler,
+                                attrs={
+                                    "svtype": "G", 
+                                    "xref": xref, 
+                                    "yref": yref,
+                                    "zref": zref,
+                                },
+                                coords={"time": [tgnss]},
+                            )
+    
+                            expected_ecef = list(gr.keplerian2ecef(struct))
+                            expected_ecef = (expected_ecef[0][0], expected_ecef[1][0], expected_ecef[2][0])
+
+                            content = "epoch, {}, ref_pos, {}, expected, {}, sv, {}, ".format(epoch, str(known_ref_pos), str(expected_ecef), sv)
                             for key in kepler.keys():
                                 content += "{}, {}, ".format(key, kepler[key])
                             fd.write(content+"\n")
