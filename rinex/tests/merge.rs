ef8ad77745aab9f6a2a892f2fa4c9c45afe6eaa8
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2023-08-29 18:40:23 +0200

    use std::pathbuf instead of strings
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- rinex/tests/merge.rs
+++ rinex/tests/merge.rs
@@ -2,50 +2,75 @@
 mod merge {
     use rinex::prelude::*;
     use rinex::Merge;
+    use std::path::PathBuf;
     #[test]
     fn merge_fail_on_type_mismatch() {
-        let test_resources = env!("CARGO_MANIFEST_DIR").to_owned() + "/../test_resources/";
-        let path1 = test_resources.to_owned() + "NAV/V3/AMEL00NLD_R_20210010000_01D_MN.rnx";
-        let mut r1 = Rinex::from_file(&path1).unwrap();
-        let path2 = test_resources.to_owned() + "OBS/V3/LARM0630.22O";
-        let r2 = Rinex::from_file(&path2).unwrap();
+        let test_resources = PathBuf::new()
+            .join(env!("CARGO_MANIFEST_DIR"))
+            .join("..")
+            .join("test_resources");
+        let path1 = test_resources
+            .clone()
+            .join("NAV")
+            .join("V3")
+            .join("AMEL00NLD_R_20210010000_01D_MN.rnx");
+        let path2 = test_resources
+            .clone()
+            .join("OBS")
+            .join("V3")
+            .join("LARM0630.22O");
+        let mut r1 = Rinex::from_file(&path1.to_string_lossy()).unwrap();
+        let r2 = Rinex::from_file(&path2.to_string_lossy()).unwrap();
         assert_eq!(r1.merge_mut(&r2).is_err(), true)
     }
     #[test]
     fn merge() {
-        let test_resources = env!("CARGO_MANIFEST_DIR").to_owned() + "/../test_resources/";
-        let path1 = test_resources.to_owned() + "NAV/V3/AMEL00NLD_R_20210010000_01D_MN.rnx";
-        let rnx_a = Rinex::from_file(&path1);
+        let test_resources = PathBuf::new()
+            .join(env!("CARGO_MANIFEST_DIR"))
+            .join("..")
+            .join("test_resources");
+        let path1 = test_resources
+            .clone()
+            .join("NAV")
+            .join("V3")
+            .join("AMEL00NLD_R_20210010000_01D_MN.rnx");
+        let rnx_a = Rinex::from_file(&path1.to_string_lossy());
         assert!(
             rnx_a.is_ok(),
-            "Failed to parse NAV/V3/AMEL00NLD_R_20210010000_01D_MN.rnx test resource"
+            "failed to parse NAV/V3/AMEL00NLD_R_20210010000_01D_MN.rnx"
         );
-        let path2 = test_resources.to_owned() + "NAV/V3/CBW100NLD_R_20210010000_01D_MN.rnx";
-        let rnx_b = Rinex::from_file(&path2);
+        let path2 = test_resources
+            .clone()
+            .join("NAV")
+            .join("V3")
+            .join("CBW100NLD_R_20210010000_01D_MN.rnx");
+        let rnx_b = Rinex::from_file(&path2.to_string_lossy());
         assert!(
             rnx_b.is_ok(),
-            "Failed to parse NAV/V3/CBW100NLD_R_20210010000_01D_MN.rnx test resource"
+            "failed to parse NAV/V3/CBW100NLD_R_20210010000_01D_MN.rnx"
         );
 
         let rnx_a = rnx_a.unwrap();
         let rnx_b = rnx_b.unwrap();
         let merged = rnx_a.merge(&rnx_b);
-        assert!(merged.is_ok(), "Failed to merge NAV/V3/CBW100NLD_R_20210010000_01D_MN.rnx into NAV/V3/AMEL00NLD_R_20210010000_01D_MN.rnx");
+        assert!(merged.is_ok(), "failed to merge NAV/V3/CBW100NLD_R_20210010000_01D_MN.rnx into NAV/V3/AMEL00NLD_R_20210010000_01D_MN.rnx");
 
         // dump
         let merged = merged.unwrap();
         assert!(
             merged.to_file("merge.txt").is_ok(),
-            "Failed to generate Merged file"
+            "failed to generate file previously merged"
         );
 
         // parse back
         let rnx = Rinex::from_file("merge.txt");
         assert!(rnx.is_ok(), "Failed to parsed back previously merged file");
 
-        //TODO
-        // this does not work, due to NAV RINEX formatting issues,
-        // unlock this in near future
+        /*
+         * Unlock reciprocity test in near future
+         *  NAV file production does not work correctly at the moment,
+         *  due to formatting issues
+         */
         // assert_eq!(rnx, merged, "Merge::ops reciprocity");
 
         let _ = std::fs::remove_file("merge.txt"); // remove file we just generated
