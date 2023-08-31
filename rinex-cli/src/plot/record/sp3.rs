c48cd007e995655c8965b9cad3042ecb6a436016
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2023-08-31 11:47:19 +0200

    plot clock data
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- rinex-cli/src/plot/record/sp3.rs
+++ rinex-cli/src/plot/record/sp3.rs
@@ -12,7 +12,7 @@ pub fn plot_sp3(ctx: &QcContext, plot_context: &mut PlotContext) {
                                         * Plot SV Position
                                         *  [+] Design 1 color per SV
                                         */
-    plot_context.add_cartesian2d_plot("High Precision Orbit (SP3)", "SV X Position (km)");
+    plot_context.add_cartesian2d_plot("High Precision Orbit (SP3)", "SV X Position [km]");
     for (sv_index, sv) in sp3.sv().enumerate() {
         let data_x: Vec<Epoch> = sp3
             .sv_position()
@@ -123,5 +123,47 @@ pub fn plot_sp3(ctx: &QcContext, plot_context: &mut PlotContext) {
             });
         plot_context.add_trace(trace);
     }
-    trace!("sp3 orbit visualization");
+    trace!("sp3 orbit data visualization");
+    /*
+     * Plot SV Clock data prediction
+     */
+    plot_context.add_cartesian2d_plot("High Precision Clock (SP3)", "SV Clock Bias [s]");
+    for (sv_index, sv) in sp3.sv().enumerate() {
+        let data_x: Vec<Epoch> = sp3
+            .sv_clock()
+            .filter_map(
+                |(epoch, svnn, _clk)| {
+                    if svnn == sv {
+                        Some(epoch)
+                    } else {
+                        None
+                    }
+                },
+            )
+            .collect();
+        let data_y: Vec<f64> = sp3
+            .sv_clock()
+            .filter_map(
+                |(_epoch, svnn, clk)| {
+                    if svnn == sv {
+                        Some(clk)
+                    } else {
+                        None
+                    }
+                },
+            )
+            .collect();
+        let trace = build_chart_epoch_axis(&format!("{}", sv), Mode::Markers, data_x, data_y)
+            .web_gl_mode(true)
+            .visible({
+                if sv_index == 0 {
+                    // Clock data differs too much: plot only one to begin with
+                    Visible::True
+                } else {
+                    Visible::LegendOnly
+                }
+            });
+        plot_context.add_trace(trace);
+    }
+    trace!("sp3 clock data visualization");
 }
