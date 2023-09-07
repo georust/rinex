6bcc8426928b925a735b2f183e5cfa602016dafe
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2023-09-07 10:05:13 +0200

    fix snr reporting
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- rinex/src/qc/analysis/obs.rs
+++ rinex/src/qc/analysis/obs.rs
@@ -1,5 +1,6 @@
 use crate::{carrier, observation::Snr, prelude::*, Carrier};
 use itertools::Itertools;
+use std::str::FromStr;
 
 use super::{pretty_array, QcOpts};
 use std::collections::HashMap;
@@ -206,8 +207,19 @@ fn report_epoch_completion(
                     }
                 }
                 tr {
-                    th {
-                        : "Complete"
+                    td {
+                        b {
+                            : "Complete"
+                        }
+                        p {
+                            : "Epochs with at least Phase + PR"
+                        }
+                        p {
+                            : "in dual frequency, with"
+                        }
+                        p {
+                            : "both SNR and elev above masks"
+                        }
                     }
                     @ for (signal, count) in complete {
                         td {
@@ -215,12 +227,8 @@ fn report_epoch_completion(
                                 : format!("L1/{}", signal)
                             }
                             p {
-                                : count.to_string()
+                                : format!("{} ({}%)", count, count * 100 / total)
                             }
-                            b {
-                                : format!("{}%", count * 100/total)
-                            }
-                            //: format!("<b>L1/{}</b>: {} (<b>{}%</b>)", signal, count, count *100/total)
                         }
                     }
                 }
@@ -235,10 +243,32 @@ fn report_snr_statistics(
     snr_stats: &HashMap<Observable, ((Epoch, f64), (Epoch, f64))>,
 ) -> Box<dyn RenderBox + '_> {
     box_html! {
-        @ for (observable, _) in snr_stats {
-            tr {
-                th {
-                    : observable.to_string()
+        tr {
+            td {
+                : ""
+            }
+            @ for (observable, _) in snr_stats {
+                @ if observable.is_phase_observable() || observable.is_pseudorange_observable() {
+                    td {
+                        : observable.to_string()
+                    }
+                }
+            }
+        }
+        tr {
+            th {
+                : "Best"
+            }
+            @ for (observable, (_, max)) in snr_stats {
+                @ if observable.is_phase_observable() || observable.is_pseudorange_observable() {
+                    td {
+                        b {
+                            : format!("{:e}", Snr::from_str(&format!("{}", max.1)).unwrap())
+                        }
+                        p {
+                            : format!("@{}", max.0)
+                        }
+                    }
                 }
             }
         }
@@ -246,9 +276,16 @@ fn report_snr_statistics(
             th {
                 : "Worst"
             }
-            @ for (_, (min, _)) in snr_stats {
-                td {
-                    : format!("{:e} @{}", min.0, min.1)
+            @ for (observable, (min, _)) in snr_stats {
+                @ if observable.is_phase_observable() || observable.is_pseudorange_observable() {
+                    td {
+                        b {
+                            : format!("{:e}", Snr::from_str(&format!("{}", min.1)).unwrap())
+                        }
+                        p {
+                            : format!("@{}", min.0)
+                        }
+                    }
                 }
             }
         }
@@ -482,29 +519,29 @@ impl QcObsAnalysis {
             .collect();
         // append snr: drop vehicle differentiation
         let mut snr: HashMap<Observable, Vec<(Epoch, f64)>> = HashMap::new();
-        for (e, _, obs, value) in rnx.snr() {
-            let value_f64: f64 = (value as u8).into();
+        for ((e, _), _, obs, snr_value) in rnx.snr() {
+            let snr_f64: f64 = (snr_value as u8).into();
             if let Some(values) = snr.get_mut(&obs) {
-                values.push((e.0, value_f64));
+                values.push((e, snr_f64));
             } else {
-                snr.insert(obs.clone(), vec![(e.0, value_f64)]);
+                snr.insert(obs.clone(), vec![(e, snr_f64)]);
             }
         }
         /*
          * SNR analysis: {min, max}
          * per signal: we do not differentiate vehicles
          */
-        let snr_stats: HashMap<Observable, ((Epoch, f64), (Epoch, f64))> = snr
-            .iter()
-            .map(|(obs, values)| {
-                let min = Statistics::min(values.1);
-                let max = Statistics::max(values.1);
-                (
-                    obs.clone(),
-                    ((Epoch::default(), min), (Epoch::default(), max)),
-                )
-            })
-            .collect();
+        let mut snr_stats: HashMap<Observable, ((Epoch, f64), (Epoch, f64))> = HashMap::new();
+        for (obs, data) in snr {
+            let values: Vec<f64> = data.iter().map(|(e, value)| *value).collect();
+            let min = values.clone().min();
+            println!("MIN: {}", min);
+            let epoch_min = data.iter().find(|(e, value)| *value == min).unwrap().0;
+            let max = values.clone().max();
+            println!("MAX: {}", max);
+            let epoch_max = data.iter().find(|(e, value)| *value == max).unwrap().0;
+            snr_stats.insert(obs, ((epoch_min, min), (epoch_max, max)));
+        }
         /*
          * sort, prior reporting
          */
