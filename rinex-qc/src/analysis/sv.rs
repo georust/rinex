c4f005a69d07db681068dfec1ec2edf917b45171
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2023-10-12 17:56:18 +0200

    improve sv reporting
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- rinex-qc/src/analysis/sv.rs
+++ rinex-qc/src/analysis/sv.rs
@@ -1,17 +1,19 @@
 use super::{pretty_array, QcOpts};
-use rinex::prelude::Rinex;
+use rinex::prelude::{Rinex, SV};
 
 use horrorshow::{box_html, RenderBox};
 use rinex_qc_traits::HtmlReport;
 
 #[derive(Debug, Clone)]
 pub struct QcSvAnalysis {
-    pub sv: Vec<String>,
+    pub sv: Vec<SV>,
 }
+
+use itertools::Itertools;
 
 impl QcSvAnalysis {
     pub fn new(primary: &Rinex, _opts: &QcOpts) -> Self {
-        let sv: Vec<_> = primary.sv().map(|sv| format!("{:X}", sv)).collect();
+        let sv: Vec<_> = primary.sv().collect();
         Self { sv }
     }
 }
@@ -27,7 +29,15 @@ impl HtmlReport for QcSvAnalysis {
                     : "PRN#"
                 }
                 td {
-                    : pretty_array(&self.sv)
+                    p {
+                        @ for mut chunks in &self.sv.iter().chunks(12) {
+                            p {
+                                @ while let Some(sv) = chunks.next() {
+                                    : format!("{:x}, ", sv)
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
