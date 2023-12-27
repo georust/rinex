b1fc216b8c766625bd7b1361a2a6bc0af1ea0d7a
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2023-12-27 19:02:27 +0100

    introduce binaries tester
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

---
+++ tools/test-binaries.sh
@@ -0,0 +1,97 @@
+#! /bin/sh
+set -e
+##############################################
+# binaries tester: to be used in CI and
+# provide at least basic means to test our CLI
+##############################################
+
+############
+# 1. CRX2RNX
+############
+./target/release/crx2rnx \
+    -f test_resources/CRNX/V3/KMS300DNK_R_20221591000_01H_30S_MO.crx.gz
+
+############
+# 2. RNX2CRX
+############
+./target/release/rnx2crx \
+    -f test_resources/OBS/V3/pdel0010.21o
+
+#############################
+# 3. OBS RINEX identification
+#############################
+./target/release/rinex-cli \
+    -f test_resources/CRNX/V3/ESBC00DNK_R_20201770000_01D_30S_MO.crx.gz \
+    -i -a
+
+#############################
+# 4. NAV RINEX identification
+#############################
+./target/release/rinex-cli \
+    -f test_resources/NAV/V3/ESBC00DNK_R_20201770000_01D_MN.rnx.gz \
+    -i -a
+
+####################
+# 5. OBS RINEX merge
+####################
+./target/release/rinex-cli \
+    -f test_resources/CRNX/V3/ESBC00DNK_R_20201770000_01D_30S_MO.crx.gz \
+    -m test_resources/CRNX/V3/MOJN00DNK_R_20201770000_01D_30S_MO.crx.gz
+
+####################
+# 6. OBS RINEX split
+####################
+./target/release/rinex-cli \
+    -f test_resources/CRNX/V3/ESBC00DNK_R_20201770000_01D_30S_MO.crx.gz \
+    -s "2020-06-25T10:00:00 UTC"
+
+###########################
+# 7. OBS RINEX time binning
+###########################
+./target/release/rinex-cli \
+    -f test_resources/CRNX/V3/ESBC00DNK_R_20201770000_01D_30S_MO.crx.gz \
+    --tbin "4 hour"
+
+######################
+# 8. GNSS combinations
+######################
+./target/release/rinex-cli \
+    -f test_resources/CRNX/V3/ESBC00DNK_R_20201770000_01D_30S_MO.crx.gz \
+    -g --if --nl --wl --mw --mp --dcb --gf
+
+#################
+# 9. OBS RINEX QC
+#################
+./target/release/rinex-cli \
+    -f test_resources/CRNX/V3/ESBC00DNK_R_20201770000_01D_30S_MO.crx.gz \
+    -Q
+
+####################################
+# 9. (advanced): state of the art QC
+####################################
+./target/release/rinex-cli \
+    -f test_resources/CRNX/V3/ESBC00DNK_R_20201770000_01D_30S_MO.crx.gz \
+    -f test_resources/NAV/V3/ESBC00DNK_R_20201770000_01D_MN.rnx.gz \
+    -f test_resources/SP3/GRG0MGXFIN_20201760000_01D_15M_ORB.SP3.gz \
+    -f test_resources/SP3/GRG0MGXFIN_20201770000_01D_15M_ORB.SP3.gz \
+    -Q
+
+####################
+# 10. (advanced) PPP
+####################
+./target/release/rinex-cli \
+    -f test_resources/CRNX/V3/ESBC00DNK_R_20201770000_01D_30S_MO.crx.gz \
+    -f test_resources/NAV/V3/ESBC00DNK_R_20201770000_01D_MN.rnx.gz \
+    -f test_resources/SP3/GRG0MGXFIN_20201760000_01D_15M_ORB.SP3.gz \
+    -f test_resources/SP3/GRG0MGXFIN_20201770000_01D_15M_ORB.SP3.gz \
+    -p
+
+###############################
+# 11. (advanced) PPP RNX2CGGTTS
+###############################
+./target/release/rinex-cli \
+    -f test_resources/CRNX/V3/ESBC00DNK_R_20201770000_01D_30S_MO.crx.gz \
+    -f test_resources/NAV/V3/ESBC00DNK_R_20201770000_01D_MN.rnx.gz \
+    -f test_resources/SP3/GRG0MGXFIN_20201760000_01D_15M_ORB.SP3.gz \
+    -f test_resources/SP3/GRG0MGXFIN_20201770000_01D_15M_ORB.SP3.gz \
+    -p --rnx2cggtts
