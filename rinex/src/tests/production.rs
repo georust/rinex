10877f509f0040a1893ea51ca060a582374e23e2
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2024-03-10 20:06:57 +0100

    unlock one more reciprocal test
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- rinex/src/tests/production.rs
+++ rinex/src/tests/production.rs
@@ -58,7 +58,6 @@ mod test {
     }
     #[test]
     #[cfg(feature = "flate2")]
-    //#[ignore]
     fn meteo_v2() {
         let folder = env!("CARGO_MANIFEST_DIR").to_owned() + "/../test_resources/MET/V2/";
         for file in std::fs::read_dir(folder).unwrap() {
