georust/rinex

commit 6fc919091c138c4d7cc92b1efc1032f817092c73

Cli improvements (#198)
* improving Cli with subcommands
* bump cggtts
* add substract opmode
* run clippy

---------

Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>
Co-authored-by: Laurențiu Nicola <lnicola@users.noreply.github.com>

rinex-cli/src/cli/mod.rs
@@ -1,0 +1,358 @@
+use clap::{value_parser, Arg, ArgAction, ArgMatches, ColorChoice, Command};
+use log::info;
+use map_3d::{ecef2geodetic, geodetic2ecef, Ellipsoid};
+use std::path::{Path, PathBuf};
+use std::str::FromStr;
+
+use crate::Error;
+use rinex::prelude::*;
+use walkdir::WalkDir;
+
+// identification mode
+mod identify;
+// graph mode
+mod graph;
+// merge mode
+mod merge;
+// split mode
+mod split;
+// tbin mode
+mod time_binning;
+// substraction mode
+mod substract;
+// QC mode
+mod qc;
+// positioning mode
+mod positioning;
+
+pub struct Cli {
+    /// Arguments passed by user
+    pub matches: ArgMatches,
+}
+
+impl Default for Cli {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+/// Context defined by User.
+pub struct Context {
+    /// Data context defined by user
+    pub data: RnxContext,
+    /// Quiet option
+    pub quiet: bool,
+    /// Workspace is the place where this session will generate data.
+    /// By default it is set to $WORKSPACE/$PRIMARYFILE.
+    /// $WORKSPACE is either manually definedd by CLI or we create it (as is).
+    /// $PRIMARYFILE is determined from the most major file contained in the dataset.
+    pub workspace: PathBuf,
+    /// (RX) reference position to be used in further analysis.
+    /// It is either (priority order is important)
+    ///  1. manually defined by CLI
+    ///  2. determined from dataset
+    pub rx_ecef: Option<(f64, f64, f64)>,
+}
+
+impl Context {
+    /*
+     * Utility to determine the most major filename stem,
+     * to be used as the session workspace
+     */
+    pub fn context_stem(data: &RnxContext) -> String {
+        let ctx_major_stem: &str = data
+            .rinex_path()
+            .expect("failed to determine a context name")
+            .file_stem()
+            .expect("failed to determine a context name")
+            .to_str()
+            .expect("failed to determine a context name");
+        /*
+         * In case $FILENAME.RNX.gz gz compressed, we extract "$FILENAME".
+         * Can use .file_name() once https://github.com/rust-lang/rust/issues/86319  is stabilized
+         */
+        let primary_stem: Vec<&str> = ctx_major_stem.split('.').collect();
+        primary_stem[0].to_string()
+    }
+    /*
+     * Creates File/Data context defined by user.
+     * Regroups all provided files/folders,
+     */
+    pub fn from_cli(cli: &Cli) -> Result<Self, Error> {
+        let mut data = RnxContext::default();
+        let max_depth = match cli.matches.get_one::<u8>("depth") {
+            Some(value) => *value as usize,
+            None => 5usize,
+        };
+
+        /* load all directories recursively, one by one */
+        for dir in cli.input_directories() {
+            let walkdir = WalkDir::new(dir).max_depth(max_depth);
+            for entry in walkdir.into_iter().filter_map(|e| e.ok()) {
+                if !entry.path().is_dir() {
+                    let filepath = entry.path().to_string_lossy().to_string();
+                    let ret = data.load(&filepath);
+                    if ret.is_err() {
+                        warn!("failed to load \"{}\": {}", filepath, ret.err().unwrap());
+                    }
+                }
+            }
+        }
+        // load individual files, if any
+        for filepath in cli.input_files() {
+            let ret = data.load(filepath);
+            if ret.is_err() {
+                warn!("failed to load \"{}\": {}", filepath, ret.err().unwrap());
+            }
+        }
+        let data_stem = Self::context_stem(&data);
+        let data_position = data.ground_position();
+        Ok(Self {
+            data,
+            quiet: cli.matches.get_flag("quiet"),
+            workspace: {
+                let path = match std::env::var("RINEX_WORKSPACE") {
+                    Ok(path) => Path::new(&path).join(data_stem).to_path_buf(),
+                    _ => match cli.matches.get_one::<PathBuf>("workspace") {
+                        Some(base_dir) => Path::new(base_dir).join(data_stem).to_path_buf(),
+                        None => Path::new("WORKSPACE").join(data_stem).to_path_buf(),
+                    },
+                };
+                // make sure the workspace is viable and exists, otherwise panic
+                std::fs::create_dir_all(&path).unwrap_or_else(|_| {
+                    panic!(
+                        "failed to create session workspace \"{}\": permission denied!",
+                        path.to_string_lossy()
+                    )
+                });
+                info!("session workspace is \"{}\"", path.to_string_lossy());
+                path
+            },
+            rx_ecef: {
+                match cli.manual_position() {
+                    Some((x, y, z)) => {
+                        let (lat, lon, _) = ecef2geodetic(x, y, z, Ellipsoid::WGS84);
+                        info!(
+                            "using manually defined position: {:?} [ECEF] (lat={:.5}°, lon={:.5}°",
+                            (x, y, z),
+                            lat,
+                            lon
+                        );
+                        Some((x, y, z))
+                    },
+                    None => {
+                        if let Some(data_pos) = data_position {
+                            let (x, y, z) = data_pos.to_ecef_wgs84();
+                            let (lat, lon, _) = ecef2geodetic(x, y, z, Ellipsoid::WGS84);
+                            info!(
+                                "position defined in dataset: {:?} [ECEF] (lat={:.5}°, lon={:.5}°",
+                                (x, y, z),
+                                lat,
+                                lon
+                            );
+                            Some((x, y, z))
+                        } else {
+                            // this is not a problem in basic opmodes,
+                            // but will most likely prevail advanced opmodes.
+                            // Let the user know.
+                            warn!("no RX position defined");
+                            None
+                        }
+                    },
+                }
+            },
+        })
+    }
+}
+
+impl Cli {
+    /// Build new command line interface
+    pub fn new() -> Self {
+        Self {
+            matches: {
+                Command::new("rinex-cli")
+                    .author("Guillaume W. Bres, <guillaume.bressaix@gmail.com>")
+                    .version(env!("CARGO_PKG_VERSION"))
+                    .about("RINEX post processing (command line)")
+                    .arg_required_else_help(true)
+                    .color(ColorChoice::Always)
+                    .arg(Arg::new("filepath")
+                        .short('f')
+                        .long("fp")
+                        .value_name("FILE")
+                        .action(ArgAction::Append)
+                        .required_unless_present("directory")
+                        .help("Input file. RINEX (any format, including Clock and ANTEX), and SP3 are accepted. You can load as many files as you need."))
+                    .arg(Arg::new("directory")
+                        .short('d')
+                        .long("dir")
+                        .value_name("DIRECTORY")
+                        .action(ArgAction::Append)
+                        .required_unless_present("filepath")
+                        .help("Load directory recursively. Default recursive depth is set to 5,
+but you can extend that with --depth.
+Again any RINEX, and SP3 are accepted. You can load as many directories as you need."))
+                    .arg(Arg::new("depth")
+                        .long("depth")
+                        .action(ArgAction::Set)
+                        .required(false)
+                        .value_parser(value_parser!(u8))
+                        .help("Extend maximal recursive search depth of -d. The default is 5."))
+                    .arg(Arg::new("quiet")
+                        .short('q')
+                        .long("quiet")
+                        .action(ArgAction::SetTrue)
+                        .help("Disable all terminal output. Also disables auto HTML reports opener."))
+                    .arg(Arg::new("workspace")
+                        .short('w')
+                        .long("workspace")
+                        .value_name("FOLDER")
+                        .value_parser(value_parser!(PathBuf))
+                        .help("Define custom workspace location. The env. variable RINEX_WORKSPACE, if present, is prefered.
+If none of those exist, we will generate local \"WORKSPACE\" folder."))
+        .next_help_heading("Preprocessing")
+            .arg(Arg::new("gps-filter")
+                .short('G')
+                .action(ArgAction::SetTrue)
+                .help("Filters out all GPS vehicles"))
+            .arg(Arg::new("glo-filter")
+                .short('R')
+                .action(ArgAction::SetTrue)
+                .help("Filters out all Glonass vehicles"))
+            .arg(Arg::new("gal-filter")
+                .short('E')
+                .action(ArgAction::SetTrue)
+                .help("Filters out all Galileo vehicles"))
+            .arg(Arg::new("bds-filter")
+                .short('C')
+                .action(ArgAction::SetTrue)
+                .help("Filters out all BeiDou vehicles"))
+            .arg(Arg::new("qzss-filter")
+                .short('J')
+                .action(ArgAction::SetTrue)
+                .help("Filters out all QZSS vehicles"))
+            .arg(Arg::new("irnss-filter")
+                .short('I')
+                .action(ArgAction::SetTrue)
+                .help("Filters out all IRNSS vehicles"))
+            .arg(Arg::new("sbas-filter")
+                .short('S')
+                .action(ArgAction::SetTrue)
+                .help("Filters out all SBAS vehicles"))
+            .arg(Arg::new("preprocessing")
+                .short('P')
+                .num_args(1..)
+                .action(ArgAction::Append)
+                .help("Filter designer. Refer to []."))
+            .arg(Arg::new("lli-mask")
+                .long("lli-mask")
+                .help("Applies given LLI AND() mask. 
+Also drops observations that did not come with an LLI flag"))
+            .next_help_heading("Receiver Antenna")
+                .arg(Arg::new("rx-ecef")
+                    .long("rx-ecef")
+                    .value_name("\"x,y,z\" coordinates in ECEF [m]")
+                    .help("Define the (RX) antenna position manually, in [m] ECEF.
+Especially if your dataset does not define such position. 
+Otherwise it gets automatically picked up."))
+                .arg(Arg::new("rx-geo")
+                    .long("rx-geo")
+                    .value_name("\"lat,lon,alt\" coordinates in ddeg [°]")
+                    .help("Define the (RX) antenna position manualy, in decimal degrees."))
+                .next_help_heading("Exclusive Opmodes: you can only run one at a time.")
+                .subcommand(graph::subcommand())
+                .subcommand(identify::subcommand())
+                .subcommand(merge::subcommand())
+                .subcommand(positioning::subcommand())
+                .subcommand(qc::subcommand())
+                .subcommand(split::subcommand())
+                .subcommand(substract::subcommand())
+                .subcommand(time_binning::subcommand())
+                .get_matches()
+            },
+        }
+    }
+    /// Returns list of input directories
+    pub fn input_directories(&self) -> Vec<&String> {
+        if let Some(fp) = self.matches.get_many::<String>("directory") {
+            fp.collect()
+        } else {
+            Vec::new()
+        }
+    }
+    /// Returns individual input filepaths
+    pub fn input_files(&self) -> Vec<&String> {
+        if let Some(fp) = self.matches.get_many::<String>("filepath") {
+            fp.collect()
+        } else {
+            Vec::new()
+        }
+    }
+    pub fn preprocessing(&self) -> Vec<&String> {
+        if let Some(filters) = self.matches.get_many::<String>("preprocessing") {
+            filters.collect()
+        } else {
+            Vec::new()
+        }
+    }
+    pub fn gps_filter(&self) -> bool {
+        self.matches.get_flag("gps-filter")
+    }
+    pub fn glo_filter(&self) -> bool {
+        self.matches.get_flag("glo-filter")
+    }
+    pub fn gal_filter(&self) -> bool {
+        self.matches.get_flag("gal-filter")
+    }
+    pub fn bds_filter(&self) -> bool {
+        self.matches.get_flag("bds-filter")
+    }
+    pub fn qzss_filter(&self) -> bool {
+        self.matches.get_flag("qzss-filter")
+    }
+    pub fn sbas_filter(&self) -> bool {
+        self.matches.get_flag("sbas-filter")
+    }
+    pub fn irnss_filter(&self) -> bool {
+        self.matches.get_flag("irnss-filter")
+    }
+    /*
+     * faillible 3D coordinates parsing
+     * it's better to panic if the descriptor is badly format
+     * then continuing with possible other coordinates than the
+     * ones desired by user
+     */
+    fn parse_3d_coordinates(desc: &String) -> (f64, f64, f64) {
+        let content = desc.split(',').collect::<Vec<&str>>();
+        if content.len() < 3 {
+            panic!("expecting x, y and z coordinates (3D)");
+        }
+        let x = f64::from_str(content[0].trim())
+            .unwrap_or_else(|_| panic!("failed to parse x coordinates"));
+        let y = f64::from_str(content[1].trim())
+            .unwrap_or_else(|_| panic!("failed to parse y coordinates"));
+        let z = f64::from_str(content[2].trim())
+            .unwrap_or_else(|_| panic!("failed to parse z coordinates"));
+        (x, y, z)
+    }
+    fn manual_ecef(&self) -> Option<(f64, f64, f64)> {
+        let desc = self.matches.get_one::<&String>("rx-ecef")?;
+        let ecef = Self::parse_3d_coordinates(desc);
+        Some(ecef)
+    }
+    fn manual_geodetic(&self) -> Option<(f64, f64, f64)> {
+        let desc = self.matches.get_one::<&String>("rx-geo")?;
+        let geo = Self::parse_3d_coordinates(desc);
+        Some(geo)
+    }
+    /// Returns RX Position possibly specified by user
+    pub fn manual_position(&self) -> Option<(f64, f64, f64)> {
+        if let Some(position) = self.manual_ecef() {
+            Some(position)
+        } else if let Some((lat, lon, alt)) = self.manual_geodetic() {
+            Some(geodetic2ecef(lat, lon, alt, Ellipsoid::WGS84))
+        } else {
+            None
+        }
+    }
+}

rinex-cli/src/graph/mod.rs
@@ -1,3 +1,9 @@
+use crate::{cli::Context, fops::open_with_web_browser, Error};
+use clap::ArgMatches;
+use rinex::observation::{Combination, Combine, Dcb};
+use std::fs::File;
+use std::io::Write;
+
 use plotly::{
     common::{
         AxisSide,
@@ -17,22 +23,23 @@ use plotly::{
 use rand::Rng;
 use serde::Serialize;
 
-use rinex::prelude::RnxContext;
 use rinex::prelude::*;
 
 mod record;
+use record::{
+    plot_atmosphere_conditions, plot_residual_ephemeris, plot_sv_nav_clock, plot_sv_nav_orbits,
+};
 
 mod context;
 pub use context::PlotContext;
 
 mod skyplot;
-pub use skyplot::skyplot;
+use skyplot::skyplot;
 
 mod naviplot;
-pub use naviplot::naviplot;
 
 mod combination;
-pub use combination::{plot_gnss_combination, plot_gnss_dcb_mp};
+use combination::{plot_gnss_code_mp, plot_gnss_combination, plot_gnss_dcb};
 
 /*
  * Generates N marker symbols to be used
@@ -495,28 +502,223 @@ pub fn build_3d_chart_epoch_label<T: Clone + Default + Serialize>(
         .hover_info(HoverInfo::All)
 }
 
-pub fn plot_record(ctx: &RnxContext, plot_ctx: &mut PlotContext) {
+/* Returns True if GNSS combination is to be plotted */
+fn gnss_combination_plot(matches: &ArgMatches) -> bool {
+    matches.get_flag("if")
+        || matches.get_flag("gf")
+        || matches.get_flag("wl")
+        || matches.get_flag("nl")
+        || matches.get_flag("mw")
+}
+
+/* Returns True if Navigation plot is to be generated */
+fn navigation_plot(matches: &ArgMatches) -> bool {
+    matches.get_flag("skyplot") || matches.get_flag("sp3-res") || matches.get_flag("sv-clock")
+}
+
+/* Returns True if Atmosphere conditions is to be generated */
+fn atmosphere_plot(matches: &ArgMatches) -> bool {
+    matches.get_flag("tropo") || matches.get_flag("tec") || matches.get_flag("ionod")
+}
+
+pub fn graph_opmode(ctx: &Context, matches: &ArgMatches) -> Result<(), Error> {
     /*
-     * Run feasible record analysis
+     * Observations graphs
      */
-    if ctx.has_observation_data() {
-        record::plot_observation(ctx, plot_ctx);
-    }
-    if let Some(data) = ctx.meteo_data() {
-        record::plot_meteo(data, plot_ctx);
-    }
-    if let Some(data) = ctx.ionex_data() {
-        if let Some(borders) = data.tec_map_borders() {
-            record::plot_tec_map(data, borders, plot_ctx);
+    if matches.get_flag("obs") {
+        let mut plot_ctx = PlotContext::new();
+        if ctx.data.has_observation_data() {
+            record::plot_observations(&ctx.data, &mut plot_ctx);
+        }
+        if let Some(data) = ctx.data.meteo_data() {
+            record::plot_meteo_observations(data, &mut plot_ctx);
+        }
+
+        /* save observations (HTML) */
+        let path = ctx.workspace.join("observations.html");
+        let mut fd =
+            File::create(&path).expect("failed to render observations (HTML): permission denied");
+
+        write!(fd, "{}", plot_ctx.to_html())
+            .expect("failed to render observations (HTML): permission denied");
+
+        info!("observations rendered in \"{}\"", path.display());
+        if !ctx.quiet {
+            open_with_web_browser(path.to_string_lossy().as_ref());
         }
     }
-    if ctx.has_navigation_data() || ctx.has_sp3() {
-        record::plot_navigation(ctx, plot_ctx);
+    /*
+     * GNSS combinations graphs
+     */
+    if gnss_combination_plot(matches) {
+        let data = ctx.data.obs_data().ok_or(Error::MissingObservationRinex)?;
+
+        let mut plot_ctx = PlotContext::new();
+        if matches.get_flag("if") {
+            let combination = data.combine(Combination::IonosphereFree);
+            plot_gnss_combination(
+                &combination,
+                &mut plot_ctx,
+                "Ionosphere Free combination",
+                "Meters of delay",
+            );
+        }
+        if matches.get_flag("gf") {
+            let combination = data.combine(Combination::GeometryFree);
+            plot_gnss_combination(
+                &combination,
+                &mut plot_ctx,
+                "Ionosphere Free combination",
+                "Meters of delay",
+            );
+        }
+        if matches.get_flag("wl") {
+            let combination = data.combine(Combination::WideLane);
+            plot_gnss_combination(
+                &combination,
+                &mut plot_ctx,
+                "Ionosphere Free combination",
+                "Meters of delay",
+            );
+        }
+        if matches.get_flag("nl") {
+            let combination = data.combine(Combination::NarrowLane);
+            plot_gnss_combination(
+                &combination,
+                &mut plot_ctx,
+                "Ionosphere Free combination",
+                "Meters of delay",
+            );
+        }
+        if matches.get_flag("mw") {
+            let combination = data.combine(Combination::MelbourneWubbena);
+            plot_gnss_combination(
+                &combination,
+                &mut plot_ctx,
+                "Ionosphere Free combination",
+                "Meters of delay",
+            );
+        }
+
+        /* save combinations (HTML) */
+        let path = ctx.workspace.join("combinations.html");
+        let mut fd = File::create(&path)
+            .expect("failed to render gnss combinations (HTML): permission denied");
+
+        write!(fd, "{}", plot_ctx.to_html())
+            .expect("failed to render gnss combinations (HTML): permission denied");
+        info!("gnss combinations rendered in \"{}\"", path.display());
+        if !ctx.quiet {
+            open_with_web_browser(path.to_string_lossy().as_ref());
+        }
     }
-    if ctx.has_sp3() && ctx.has_navigation_data() {
-        record::plot_residual_ephemeris(ctx, plot_ctx);
+    /*
+     * DCB visualization
+     */
+    if matches.get_flag("dcb") {
+        let data = ctx.data.obs_data().ok_or(Error::MissingObservationRinex)?;
+
+        let mut plot_ctx = PlotContext::new();
+        let data = data.dcb();
+        plot_gnss_dcb(
+            &data,
+            &mut plot_ctx,
+            "Differential Code Bias",
+            "Differential Code Bias [s]",
+        );
+
+        /* save dcb (HTML) */
+        let path = ctx.workspace.join("dcb.html");
+        let mut fd = File::create(&path).expect("failed to render dcb (HTML): permission denied");
+
+        write!(fd, "{}", plot_ctx.to_html())
+            .expect("failed to render dcb (HTML): permission denied");
+        info!("dcb graph rendered in \"{}\"", path.display());
+        if !ctx.quiet {
+            open_with_web_browser(path.to_string_lossy().as_ref());
+        }
     }
-    if ctx.has_navigation_data() {
-        record::plot_ionospheric_delay(ctx, plot_ctx);
+    if matches.get_flag("mp") {
+        let data = ctx.data.obs_data().ok_or(Error::MissingObservationRinex)?;
+
+        let mut plot_ctx = PlotContext::new();
+        let data = data.code_multipath();
+        plot_gnss_code_mp(&data, &mut plot_ctx, "Code Multipath", "Meters of delay");
+
+        /* save multipath (HTML) */
+        let path = ctx.workspace.join("multipath.html");
+        let mut fd =
+            File::create(&path).expect("failed to render multipath (HTML): permission denied");
+
+        write!(fd, "{}", plot_ctx.to_html())
+            .expect("failed to render multiath (HTML): permission denied");
+        info!("code multipath rendered in \"{}\"", path.display());
+        if !ctx.quiet {
+            open_with_web_browser(path.to_string_lossy().as_ref());
+        }
     }
+    if navigation_plot(matches) {
+        let mut plot_ctx = PlotContext::new();
+
+        if matches.get_flag("skyplot") {
+            let rx_ecef = ctx
+                .rx_ecef
+                .expect("skyplot requires the receiver location to be defined.");
+            if ctx.data.sp3_data().is_none() && ctx.data.nav_data().is_none() {
+                panic!("skyplot requires either BRDC or SP3.");
+            }
+            skyplot(&ctx.data, rx_ecef, &mut plot_ctx);
+        }
+        if matches.get_flag("orbits") {
+            plot_sv_nav_orbits(&ctx.data, &mut plot_ctx);
+        }
+        if matches.get_flag("sp3-res") {
+            if ctx.data.sp3_data().is_none() || ctx.data.nav_data().is_none() {
+                panic!("skyplot requires both BRDC or SP3.");
+            }
+            plot_residual_ephemeris(&ctx.data, &mut plot_ctx);
+        }
+        /* save navigation (HTML) */
+        let path = ctx.workspace.join("navigation.html");
+        let mut fd =
+            File::create(&path).expect("failed to render navigation (HTML): permission denied");
+
+        write!(fd, "{}", plot_ctx.to_html())
+            .expect("failed to render navigation (HTML): permission denied");
+        info!("code multipath rendered in \"{}\"", path.display());
+        if !ctx.quiet {
+            open_with_web_browser(path.to_string_lossy().as_ref());
+        }
+    }
+    if matches.get_flag("sv-clock") {
+        let mut plot_ctx = PlotContext::new();
+        plot_sv_nav_clock(&ctx.data, &mut plot_ctx);
+        /* save clock states (HTML) */
+        let path = ctx.workspace.join("clocks.html");
+        let mut fd =
+            File::create(&path).expect("failed to render clock states (HTML): permission denied");
+
+        write!(fd, "{}", plot_ctx.to_html())
+            .expect("failed to render clock states (HTML): permission denied");
+        info!("clock graphs rendered in \"{}\"", path.display());
+        if !ctx.quiet {
+            open_with_web_browser(path.to_string_lossy().as_ref());
+        }
+    }
+    if atmosphere_plot(matches) {
+        let mut plot_ctx = PlotContext::new();
+        plot_atmosphere_conditions(ctx, &mut plot_ctx, matches);
+        /* save (HTML) */
+        let path = ctx.workspace.join("atmosphere.html");
+        let mut fd = File::create(&path)
+            .expect("failed to render atmosphere graphs (HTML): permission denied");
+
+        write!(fd, "{}", plot_ctx.to_html())
+            .expect("failed to render atmosphere graphs (HTML): permission denied");
+        info!("atmosphere graphs rendered in \"{}\"", path.display());
+        if !ctx.quiet {
+            open_with_web_browser(path.to_string_lossy().as_ref());
+        }
+    }
+    Ok(())
 }

rinex-cli/src/graph/record/mod.rs
@@ -1,0 +1,32 @@
+mod ionex;
+mod ionosphere;
+mod meteo;
+mod navigation;
+mod observation;
+mod sp3_plot;
+
+pub use meteo::plot_meteo_observations;
+pub use navigation::plot_sv_nav_clock;
+pub use navigation::plot_sv_nav_orbits;
+pub use observation::plot_observations;
+pub use sp3_plot::plot_residual_ephemeris;
+
+use crate::cli::Context;
+use crate::graph::PlotContext;
+use clap::ArgMatches;
+
+use ionex::plot_tec_map;
+use ionosphere::plot_ionospheric_delay;
+
+pub fn plot_atmosphere_conditions(ctx: &Context, plot_ctx: &mut PlotContext, matches: &ArgMatches) {
+    if matches.get_flag("tropo") {
+        let _meteo = ctx.data.meteo_data().expect("--tropo requires METEO RINEX");
+    }
+    if matches.get_flag("ionod") {
+        plot_ionospheric_delay(&ctx.data, plot_ctx);
+    }
+    if matches.get_flag("tec") {
+        let ionex = ctx.data.ionex_data().expect("--tec required IONEX");
+        plot_tec_map(ionex, ((0.0_f64, 0.0_f64), (0.0_f64, 0.0_f64)), plot_ctx);
+    }
+}

rinex-cli/src/plot/record/mod.rs
@@ -1,13 +1,0 @@
-mod ionex;
-mod ionosphere;
-mod meteo;
-mod navigation;
-mod observation;
-mod sp3_plot;
-
-pub use ionex::plot_tec_map;
-pub use ionosphere::plot_ionospheric_delay;
-pub use meteo::plot_meteo;
-pub use navigation::plot_navigation;
-pub use observation::plot_observation;
-pub use sp3_plot::plot_residual_ephemeris;

commit afd3546a71d5274fe085883ff7927a24c36629ff

Correct issue with using rx-ecef and rx-geo options
Rust panics with a variation of following error when trying to pass
either of these options to rinex-cli:

~~~
Mismatch between definition and access of `rx-ecef`. Could not downcast to
&alloc::string::String, need to downcast to alloc::string::String
~~~

Fixes: 6fc919091c13

rinex-cli/src/cli/mod.rs
@@ -283,12 +283,12 @@ Otherwise it gets automatically picked up."))
         (x, y, z)
     }
     fn manual_ecef(&self) -> Option<(f64, f64, f64)> {
-        let desc = self.matches.get_one::<&String>("rx-ecef")?;
+        let desc = self.matches.get_one::<String>("rx-ecef")?;
         let ecef = Self::parse_3d_coordinates(desc);
         Some(ecef)
     }
     fn manual_geodetic(&self) -> Option<(f64, f64, f64)> {
-        let desc = self.matches.get_one::<&String>("rx-geo")?;
+        let desc = self.matches.get_one::<String>("rx-geo")?;
         let geo = Self::parse_3d_coordinates(desc);
         Some(geo)
     }