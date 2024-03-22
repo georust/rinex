georust/rinex

commit 6fc919091c138c4d7cc92b1efc1032f817092c73

Cli improvements (#198)
* improving Cli with subcommands
* bump cggtts
* add substract opmode
* run clippy

---------

Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>
Co-authored-by: Laurențiu Nicola <lnicola@users.noreply.github.com>

rinex-cli/src/cli/identify.rs
@@ -1,0 +1,77 @@
+// Data identification opmode
+use clap::{Arg, ArgAction, Command};
+
+pub fn subcommand() -> Command {
+    Command::new("identify")
+        .short_flag('i')
+        .long_flag("id")
+        .arg_required_else_help(true)
+        .about("RINEX data identification opmode")
+        .arg(
+            Arg::new("all").short('a').action(ArgAction::SetTrue).help(
+                "Complete RINEX dataset(s) identification. Turns on all following algorithms.",
+            ),
+        )
+        .arg(
+            Arg::new("epochs")
+                .long("epochs")
+                .short('e')
+                .action(ArgAction::SetTrue)
+                .help("Epoch, Time system and sampling analysis."),
+        )
+        .arg(
+            Arg::new("gnss")
+                .long("gnss")
+                .short('g')
+                .action(ArgAction::SetTrue)
+                .help("Enumerate GNSS constellations."),
+        )
+        .arg(
+            Arg::new("sv")
+                .long("sv")
+                .short('s')
+                .action(ArgAction::SetTrue)
+                .help("Enumerates SV."),
+        )
+        .arg(
+            Arg::new("header")
+                .long("header")
+                .short('h')
+                .action(ArgAction::SetTrue)
+                .help("Extracts major header fields"),
+        )
+        .next_help_heading(
+            "Following sections are RINEX specific. They will only apply to the related subset.",
+        )
+        .next_help_heading("Observation RINEX")
+        .arg(
+            Arg::new("observables")
+                .long("obs")
+                .short('o')
+                .action(ArgAction::SetTrue)
+                .help("Identify observables in either Observation or Meteo dataset(s)."),
+        )
+        .arg(
+            Arg::new("snr")
+                .long("snr")
+                .action(ArgAction::SetTrue)
+                .help("SNR identification ([min, max] range, per SV..)"),
+        )
+        .arg(
+            Arg::new("anomalies")
+                .short('a')
+                .long("anomalies")
+                .action(ArgAction::SetTrue)
+                .help(
+                    "Enumerate abnormal events along the input time frame (epochs).
+           Abnormal events are unexpected receiver reset or possible cycle slips for example.",
+                ),
+        )
+        .next_help_heading("Navigation (BRDC) RINEX")
+        .arg(
+            Arg::new("nav-msg")
+                .long("nav-msg")
+                .action(ArgAction::SetTrue)
+                .help("Identify Navigation frame types."),
+        )
+}

.github/workflows/rust.yml
@@ -36,8 +36,8 @@ jobs:
         cargo install cargo-audit
         cargo audit
 
-  tests:
-    name: Tests 
+  build:
+    name: Build
     runs-on: ubuntu-latest
     steps:
     - uses: actions/checkout@v3
@@ -46,20 +46,29 @@ jobs:
       with:
         toolchain: stable
         override: true
+    
     - name: UBX2RNX dependencies 
       run: |
         sudo apt-get update
         sudo apt-get install -y libudev-dev
+    
     - uses: actions-rs/cargo@v1
       name: Test
       with:
         command: test
         args: --verbose
+    
     - uses: actions-rs/cargo@v1
       name: Test (all features)
       with:
         command: test
         args: --verbose --all-features
+    
+    - uses: actions-rs/cargo@v1
+      name: Build (all features)
+      with:
+        command: build
+        args: --all-features --release
   
   windows-build:
     runs-on: windows-latest
@@ -70,26 +79,18 @@ jobs:
       with:
         toolchain: stable
         override: true
+        
     - uses: actions-rs/cargo@v1
-      name: RINEXCLI 
+      name: Build (default) 
       with:
         command: build
-        args: -p rinex-cli --all-features --release --verbose
+        args: --release --verbose
+
     - uses: actions-rs/cargo@v1
-      name: RNX2CGGTS
-      with:
-        command: build
-        args: -p rnx2cggtts --all-features --release --verbose
-    - uses: actions-rs/cargo@v1
-      name: RNX2CRNX
+      name: Build (all features)
       with: 
         command: build 
-        args: -p rnx2crx --all-features --release --verbose
-    - uses: actions-rs/cargo@v1
-      name: CRX2RNX
-      with: 
-        command: build
-        args: -p crx2rnx --all-features --release --verbose
+        args: --all-features --release --verbose
   
   macos-build:
     runs-on: macos-latest
@@ -100,23 +101,15 @@ jobs:
       with:
         toolchain: stable
         override: true
+    
     - uses: actions-rs/cargo@v1
-      name: RINEXCLI 
+      name: Build (default) 
       with:
         command: build
-        args: -p rinex-cli --all-features --release --verbose
+        args: --release --verbose
+
     - uses: actions-rs/cargo@v1
-      name: RNX2CGGTS
-      with:
-        command: build
-        args: -p rnx2cggtts --all-features --release --verbose
-    - uses: actions-rs/cargo@v1
-      name: RNX2CRNX
+      name: Build (all features)
       with: 
         command: build 
-        args: -p rnx2crx --all-features --release --verbose
-    - uses: actions-rs/cargo@v1
-      name: CRX2RNX
-      with: 
-        command: build
-        args: -p crx2rnx --all-features --release --verbose
+        args: --all-features --release --verbose

Cargo.toml
@@ -7,7 +7,6 @@ members = [
     "rinex", 
     "rinex-qc", 
     "rinex-cli", 
-    "rnx2cggtts", 
     "rnx2crx", 
     "sinex",
     "sp3",

crx2rnx/Cargo.toml
@@ -12,4 +12,4 @@ readme = "README.md"
 
 [dependencies]
 clap = { version = "4.4.10", features = ["derive", "color"] }
-rinex = { path = "../rinex", version = "=0.15.2", features = ["serde"] }
+rinex = { path = "../rinex", version = "=0.15.3", features = ["serde"] }

rinex-cli/Cargo.toml
@@ -1,6 +1,6 @@
 [package]
 name = "rinex-cli"
-version = "0.9.7"
+version = "0.10.0"
 license = "MIT OR Apache-2.0"
 authors = ["Guillaume W. Bres <guillaume.bressaix@gmail.com>"]
 description = "Command line tool parse and analyze RINEX data"
@@ -28,11 +28,11 @@ map_3d = "0.1.5"
 # ndarray = "0.15"
 colorous = "1.0"
 horrorshow = "0.8"
-clap = { version = "4.4.10", features = ["derive", "color"] }
+clap = { version = "4.4.11", features = ["derive", "color"] }
 hifitime = { version = "3.8.4", features = ["serde", "std"] }
 gnss-rs = { version = "2.1.2" , features = ["serde"] }
-rinex = { path = "../rinex", version = "=0.15.2", features = ["full"] }
-rinex-qc = { path = "../rinex-qc", version = "=0.1.7", features = ["serde"] } 
+rinex = { path = "../rinex", version = "=0.15.3", features = ["full"] }
+rinex-qc = { path = "../rinex-qc", version = "=0.1.8", features = ["serde"] } 
 sp3 = { path = "../sp3", version = "=1.0.6",  features = ["serde", "flate2"] }
 serde = { version = "1.0", default-features = false, features = ["derive"] }
 
@@ -44,3 +44,8 @@ plotly =  "0.8.4"
 gnss-rtk = { version = "0.4.1", features = ["serde"] }
 # gnss-rtk = { git = "https://github.com/rtk-rs/gnss-rtk", branch = "develop", features = ["serde"] }
 # gnss-rtk = { path = "../../rtk-rs/gnss-rtk", features = ["serde"] }
+
+# cggtts
+cggtts = { version = "4.1.2", features = ["serde", "scheduler"] }
+# cggtts = { git = "https://github.com/gwbres/cggtts", branch = "develop", features = ["serde", "scheduler"] }
+# cggtts = { path = "../../cggtts/cggtts", features = ["serde", "scheduler"] }

rinex-cli/src/analysis/sampling.rs
@@ -1,4 +1,4 @@
-use crate::plot::PlotContext;
+use crate::graph::PlotContext;
 use itertools::Itertools;
 use plotly::Histogram; //.sorted()
 use rinex::prelude::RnxContext;

rinex-cli/src/cli/graph.rs
@@ -1,0 +1,142 @@
+use clap::{Arg, ArgAction, Command};
+
+pub fn subcommand() -> Command {
+    Command::new("graph")
+        .short_flag('g')
+        .long_flag("graph")
+        .arg_required_else_help(true)
+        .about(
+            "RINEX dataset visualization (signals, orbits..), rendered as HTML in the workspace.",
+        )
+        .next_help_heading(
+            "RINEX dependent visualizations. 
+        Will only generate graphs if related dataset is present.",
+        )
+        .next_help_heading("GNSS observations (requires OBS RINEX)")
+        .arg(
+            Arg::new("obs")
+                .short('o')
+                .long("obs")
+                .action(ArgAction::SetTrue)
+                .help(
+                    "Plot all observables.
+When OBS RINEX is provided, this will plot raw phase, dopplers and SSI.
+When METEO RINEX is provided, data from meteo sensors is plotted too.",
+                ),
+        )
+        .arg(
+            Arg::new("dcb")
+                .long("dcb")
+                .action(ArgAction::SetTrue)
+                .help("Plot Differential Code Bias. Requires OBS RINEX."),
+        )
+        .arg(
+            Arg::new("mp")
+                .long("mp")
+                .action(ArgAction::SetTrue)
+                .help("Plot Code Multipath. Requires OBS RINEX."),
+        )
+        .next_help_heading("GNSS combinations (requires OBS RINEX)")
+        .arg(
+            Arg::new("if")
+                .short('i')
+                .long("if")
+                .action(ArgAction::SetTrue)
+                .help("Plot Ionosphere Free (IF) signal combination."),
+        )
+        .arg(
+            Arg::new("gf")
+                .long("gf")
+                .short('g')
+                .action(ArgAction::SetTrue)
+                .conflicts_with("no-graph")
+                .help("Plot Geometry Free (GF) signal combination."),
+        )
+        .arg(
+            Arg::new("wl")
+                .long("wl")
+                .short('w')
+                .action(ArgAction::SetTrue)
+                .help("Plot Wide Lane (WL) signal combination."),
+        )
+        .arg(
+            Arg::new("nl")
+                .long("nl")
+                .short('n')
+                .action(ArgAction::SetTrue)
+                .conflicts_with("no-graph")
+                .help("Plot Narrow Lane (WL) signal combination."),
+        )
+        .arg(
+            Arg::new("mw")
+                .long("mw")
+                .short('m')
+                .action(ArgAction::SetTrue)
+                .conflicts_with("no-graph")
+                .help("Plot Melbourne-Wübbena (MW) signal combination."),
+        )
+        .arg(Arg::new("cs").long("cs").action(ArgAction::SetTrue).help(
+            "Phase / Cycle Slip graph.
+Plots raw phase signal with blackened sample where either CS was declared by receiver,
+or we post processed determined a CS.",
+        ))
+        .next_help_heading("Navigation (requires NAV RINEX and/or SP3)")
+        .arg(
+            Arg::new("skyplot")
+                .short('s')
+                .long("sky")
+                .action(ArgAction::SetTrue)
+                .help("Skyplot: SV position in the sky, on a compass."),
+        )
+        .arg(
+            Arg::new("orbits")
+                .long("orbits")
+                .action(ArgAction::SetTrue)
+                .help("SV position in the sky, on 2D cartesian plots."),
+        )
+        .arg(
+            Arg::new("sp3-res")
+                .long("sp3-res")
+                .action(ArgAction::SetTrue)
+                .help(
+                    "SV orbital attitude residual analysis |BRDC - SP3|.
+Requires both NAV RINEX and SP3 that overlap in time.",
+                ),
+        )
+        .arg(
+            Arg::new("naviplot")
+                .long("naviplot")
+                .action(ArgAction::SetTrue)
+                .help(
+                    "SV orbital attitude projected in 3D.
+Ideal for precise positioning decision making.",
+                ),
+        )
+        .next_help_heading("Clock states (requires either NAV RINEX, CLK RINEX or SP3)")
+        .arg(
+            Arg::new("sv-clock")
+                .short('c')
+                .long("clk")
+                .action(ArgAction::SetTrue)
+                .help("SV clock bias (offset, drift, drift changes)."),
+        )
+        .next_help_heading("Atmosphere conditions")
+        .arg(
+            Arg::new("tropo")
+                .long("tropo")
+                .action(ArgAction::SetTrue)
+                .help("Plot tropospheric delay from meteo sensors estimation. Requires METEO RINEX."),
+        )
+        .arg(
+            Arg::new("tec")
+                .long("tec")
+                .action(ArgAction::SetTrue)
+                .help("Plot the TEC map. Requires at least one IONEX file."),
+        )
+        .arg(
+            Arg::new("ionod")
+                .long("ionod")
+                .action(ArgAction::SetTrue)
+                .help("Plot ionospheric delay per signal & SV, at latitude and longitude of signal sampling."),
+        )
+}

commit 86fc76a58d8679c0ca324c05eec1e2708e91daea

Correct issues with arguments to rinex-cli's identify command
Fixes several errors that are encountered when trying to use the identify
command:

  - The "all" and "anomalies" arguments were using the same short
    option identifier 'a'. The former has been renamed to 'A'.

  - The "header" argument had a short option identifier 'h' that
    conflicted with the implicit "help". The short option has been
    removed for "header".

  - The "station" argument was duplicated in its entirety

Fixes: 6fc919091c13

rinex-cli/src/cli/identify.rs
@@ -8,7 +8,7 @@ pub fn subcommand() -> Command {
         .arg_required_else_help(true)
         .about("RINEX data identification opmode")
         .arg(
-            Arg::new("all").short('a').action(ArgAction::SetTrue).help(
+            Arg::new("all").short('A').action(ArgAction::SetTrue).help(
                 "Complete RINEX dataset(s) identification. Turns on all following algorithms.",
             ),
         )
@@ -36,7 +36,6 @@ pub fn subcommand() -> Command {
         .arg(
             Arg::new("header")
                 .long("header")
-                .short('h')
                 .action(ArgAction::SetTrue)
                 .help("Extracts major header fields"),
         )
@@ -75,12 +74,6 @@ pub fn subcommand() -> Command {
                 .help("Identify Navigation frame types."),
         )
         .next_help_heading("Clock RINEX")
-        .arg(
-            Arg::new("station")
-                .long("station")
-                .action(ArgAction::SetTrue)
-                .help("Identify all ground stations contained in CLK RINEX file(s)"),
-        )
         .arg(
             Arg::new("station")
                 .long("station")