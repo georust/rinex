georust/rinex

commit 6fc919091c138c4d7cc92b1efc1032f817092c73

Cli improvements (#198)
* improving Cli with subcommands
* bump cggtts
* add substract opmode
* run clippy

---------

Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>
Co-authored-by: Laurențiu Nicola <lnicola@users.noreply.github.com>

rinex-cli/src/cli/graph.rs
@@ -1,0 +1,142 @@
+use clap::{Arg, ArgAction, Command};
+
+pub fn subcommand() -> Command {
+    Command::new("graph")
+        .short_flag('g')
+        .long_flag("graph")
+        .arg_required_else_help(true)
+        .about(
+            "RINEX dataset visualization (signals, orbits..), rendered as HTML in the workspace.",
+        )
+        .next_help_heading(
+            "RINEX dependent visualizations. 
+        Will only generate graphs if related dataset is present.",
+        )
+        .next_help_heading("GNSS observations (requires OBS RINEX)")
+        .arg(
+            Arg::new("obs")
+                .short('o')
+                .long("obs")
+                .action(ArgAction::SetTrue)
+                .help(
+                    "Plot all observables.
+When OBS RINEX is provided, this will plot raw phase, dopplers and SSI.
+When METEO RINEX is provided, data from meteo sensors is plotted too.",
+                ),
+        )
+        .arg(
+            Arg::new("dcb")
+                .long("dcb")
+                .action(ArgAction::SetTrue)
+                .help("Plot Differential Code Bias. Requires OBS RINEX."),
+        )
+        .arg(
+            Arg::new("mp")
+                .long("mp")
+                .action(ArgAction::SetTrue)
+                .help("Plot Code Multipath. Requires OBS RINEX."),
+        )
+        .next_help_heading("GNSS combinations (requires OBS RINEX)")
+        .arg(
+            Arg::new("if")
+                .short('i')
+                .long("if")
+                .action(ArgAction::SetTrue)
+                .help("Plot Ionosphere Free (IF) signal combination."),
+        )
+        .arg(
+            Arg::new("gf")
+                .long("gf")
+                .short('g')
+                .action(ArgAction::SetTrue)
+                .conflicts_with("no-graph")
+                .help("Plot Geometry Free (GF) signal combination."),
+        )
+        .arg(
+            Arg::new("wl")
+                .long("wl")
+                .short('w')
+                .action(ArgAction::SetTrue)
+                .help("Plot Wide Lane (WL) signal combination."),
+        )
+        .arg(
+            Arg::new("nl")
+                .long("nl")
+                .short('n')
+                .action(ArgAction::SetTrue)
+                .conflicts_with("no-graph")
+                .help("Plot Narrow Lane (WL) signal combination."),
+        )
+        .arg(
+            Arg::new("mw")
+                .long("mw")
+                .short('m')
+                .action(ArgAction::SetTrue)
+                .conflicts_with("no-graph")
+                .help("Plot Melbourne-Wübbena (MW) signal combination."),
+        )
+        .arg(Arg::new("cs").long("cs").action(ArgAction::SetTrue).help(
+            "Phase / Cycle Slip graph.
+Plots raw phase signal with blackened sample where either CS was declared by receiver,
+or we post processed determined a CS.",
+        ))
+        .next_help_heading("Navigation (requires NAV RINEX and/or SP3)")
+        .arg(
+            Arg::new("skyplot")
+                .short('s')
+                .long("sky")
+                .action(ArgAction::SetTrue)
+                .help("Skyplot: SV position in the sky, on a compass."),
+        )
+        .arg(
+            Arg::new("orbits")
+                .long("orbits")
+                .action(ArgAction::SetTrue)
+                .help("SV position in the sky, on 2D cartesian plots."),
+        )
+        .arg(
+            Arg::new("sp3-res")
+                .long("sp3-res")
+                .action(ArgAction::SetTrue)
+                .help(
+                    "SV orbital attitude residual analysis |BRDC - SP3|.
+Requires both NAV RINEX and SP3 that overlap in time.",
+                ),
+        )
+        .arg(
+            Arg::new("naviplot")
+                .long("naviplot")
+                .action(ArgAction::SetTrue)
+                .help(
+                    "SV orbital attitude projected in 3D.
+Ideal for precise positioning decision making.",
+                ),
+        )
+        .next_help_heading("Clock states (requires either NAV RINEX, CLK RINEX or SP3)")
+        .arg(
+            Arg::new("sv-clock")
+                .short('c')
+                .long("clk")
+                .action(ArgAction::SetTrue)
+                .help("SV clock bias (offset, drift, drift changes)."),
+        )
+        .next_help_heading("Atmosphere conditions")
+        .arg(
+            Arg::new("tropo")
+                .long("tropo")
+                .action(ArgAction::SetTrue)
+                .help("Plot tropospheric delay from meteo sensors estimation. Requires METEO RINEX."),
+        )
+        .arg(
+            Arg::new("tec")
+                .long("tec")
+                .action(ArgAction::SetTrue)
+                .help("Plot the TEC map. Requires at least one IONEX file."),
+        )
+        .arg(
+            Arg::new("ionod")
+                .long("ionod")
+                .action(ArgAction::SetTrue)
+                .help("Plot ionospheric delay per signal & SV, at latitude and longitude of signal sampling."),
+        )
+}

rinex-cli/src/cli/identify.rs
@@ -1,0 +1,77 @@
+// Data identification opmode
+use clap::{Arg, ArgAction, Command};
+
+pub fn subcommand() -> Command {
+    Command::new("identify")
+        .short_flag('i')
+        .long_flag("id")
+        .arg_required_else_help(true)
+        .about("RINEX data identification opmode")
+        .arg(
+            Arg::new("all").short('a').action(ArgAction::SetTrue).help(
+                "Complete RINEX dataset(s) identification. Turns on all following algorithms.",
+            ),
+        )
+        .arg(
+            Arg::new("epochs")
+                .long("epochs")
+                .short('e')
+                .action(ArgAction::SetTrue)
+                .help("Epoch, Time system and sampling analysis."),
+        )
+        .arg(
+            Arg::new("gnss")
+                .long("gnss")
+                .short('g')
+                .action(ArgAction::SetTrue)
+                .help("Enumerate GNSS constellations."),
+        )
+        .arg(
+            Arg::new("sv")
+                .long("sv")
+                .short('s')
+                .action(ArgAction::SetTrue)
+                .help("Enumerates SV."),
+        )
+        .arg(
+            Arg::new("header")
+                .long("header")
+                .short('h')
+                .action(ArgAction::SetTrue)
+                .help("Extracts major header fields"),
+        )
+        .next_help_heading(
+            "Following sections are RINEX specific. They will only apply to the related subset.",
+        )
+        .next_help_heading("Observation RINEX")
+        .arg(
+            Arg::new("observables")
+                .long("obs")
+                .short('o')
+                .action(ArgAction::SetTrue)
+                .help("Identify observables in either Observation or Meteo dataset(s)."),
+        )
+        .arg(
+            Arg::new("snr")
+                .long("snr")
+                .action(ArgAction::SetTrue)
+                .help("SNR identification ([min, max] range, per SV..)"),
+        )
+        .arg(
+            Arg::new("anomalies")
+                .short('a')
+                .long("anomalies")
+                .action(ArgAction::SetTrue)
+                .help(
+                    "Enumerate abnormal events along the input time frame (epochs).
+           Abnormal events are unexpected receiver reset or possible cycle slips for example.",
+                ),
+        )
+        .next_help_heading("Navigation (BRDC) RINEX")
+        .arg(
+            Arg::new("nav-msg")
+                .long("nav-msg")
+                .action(ArgAction::SetTrue)
+                .help("Identify Navigation frame types."),
+        )
+}

rinex-cli/src/cli/mod.rs
@@ -1,0 +1,358 @@
+use clap::{value_parser, Arg, ArgAction, ArgMatches, ColorChoice, Command};
+use log::info;
+use map_3d::{ecef2geodetic, geodetic2ecef, Ellipsoid};
+use std::path::{Path, PathBuf};
+use std::str::FromStr;
+
+use crate::Error;
+use rinex::prelude::*;
+use walkdir::WalkDir;
+
+// identification mode
+mod identify;
+// graph mode
+mod graph;
+// merge mode
+mod merge;
+// split mode
+mod split;
+// tbin mode
+mod time_binning;
+// substraction mode
+mod substract;
+// QC mode
+mod qc;
+// positioning mode
+mod positioning;
+
+pub struct Cli {
+    /// Arguments passed by user
+    pub matches: ArgMatches,
+}
+
+impl Default for Cli {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+/// Context defined by User.
+pub struct Context {
+    /// Data context defined by user
+    pub data: RnxContext,
+    /// Quiet option
+    pub quiet: bool,
+    /// Workspace is the place where this session will generate data.
+    /// By default it is set to $WORKSPACE/$PRIMARYFILE.
+    /// $WORKSPACE is either manually definedd by CLI or we create it (as is).
+    /// $PRIMARYFILE is determined from the most major file contained in the dataset.
+    pub workspace: PathBuf,
+    /// (RX) reference position to be used in further analysis.
+    /// It is either (priority order is important)
+    ///  1. manually defined by CLI
+    ///  2. determined from dataset
+    pub rx_ecef: Option<(f64, f64, f64)>,
+}
+
+impl Context {
+    /*
+     * Utility to determine the most major filename stem,
+     * to be used as the session workspace
+     */
+    pub fn context_stem(data: &RnxContext) -> String {
+        let ctx_major_stem: &str = data
+            .rinex_path()
+            .expect("failed to determine a context name")
+            .file_stem()
+            .expect("failed to determine a context name")
+            .to_str()
+            .expect("failed to determine a context name");
+        /*
+         * In case $FILENAME.RNX.gz gz compressed, we extract "$FILENAME".
+         * Can use .file_name() once https://github.com/rust-lang/rust/issues/86319  is stabilized
+         */
+        let primary_stem: Vec<&str> = ctx_major_stem.split('.').collect();
+        primary_stem[0].to_string()
+    }
+    /*
+     * Creates File/Data context defined by user.
+     * Regroups all provided files/folders,
+     */
+    pub fn from_cli(cli: &Cli) -> Result<Self, Error> {
+        let mut data = RnxContext::default();
+        let max_depth = match cli.matches.get_one::<u8>("depth") {
+            Some(value) => *value as usize,
+            None => 5usize,
+        };
+
+        /* load all directories recursively, one by one */
+        for dir in cli.input_directories() {
+            let walkdir = WalkDir::new(dir).max_depth(max_depth);
+            for entry in walkdir.into_iter().filter_map(|e| e.ok()) {
+                if !entry.path().is_dir() {
+                    let filepath = entry.path().to_string_lossy().to_string();
+                    let ret = data.load(&filepath);
+                    if ret.is_err() {
+                        warn!("failed to load \"{}\": {}", filepath, ret.err().unwrap());
+                    }
+                }
+            }
+        }
+        // load individual files, if any
+        for filepath in cli.input_files() {
+            let ret = data.load(filepath);
+            if ret.is_err() {
+                warn!("failed to load \"{}\": {}", filepath, ret.err().unwrap());
+            }
+        }
+        let data_stem = Self::context_stem(&data);
+        let data_position = data.ground_position();
+        Ok(Self {
+            data,
+            quiet: cli.matches.get_flag("quiet"),
+            workspace: {
+                let path = match std::env::var("RINEX_WORKSPACE") {
+                    Ok(path) => Path::new(&path).join(data_stem).to_path_buf(),
+                    _ => match cli.matches.get_one::<PathBuf>("workspace") {
+                        Some(base_dir) => Path::new(base_dir).join(data_stem).to_path_buf(),
+                        None => Path::new("WORKSPACE").join(data_stem).to_path_buf(),
+                    },
+                };
+                // make sure the workspace is viable and exists, otherwise panic
+                std::fs::create_dir_all(&path).unwrap_or_else(|_| {
+                    panic!(
+                        "failed to create session workspace \"{}\": permission denied!",
+                        path.to_string_lossy()
+                    )
+                });
+                info!("session workspace is \"{}\"", path.to_string_lossy());
+                path
+            },
+            rx_ecef: {
+                match cli.manual_position() {
+                    Some((x, y, z)) => {
+                        let (lat, lon, _) = ecef2geodetic(x, y, z, Ellipsoid::WGS84);
+                        info!(
+                            "using manually defined position: {:?} [ECEF] (lat={:.5}°, lon={:.5}°",
+                            (x, y, z),
+                            lat,
+                            lon
+                        );
+                        Some((x, y, z))
+                    },
+                    None => {
+                        if let Some(data_pos) = data_position {
+                            let (x, y, z) = data_pos.to_ecef_wgs84();
+                            let (lat, lon, _) = ecef2geodetic(x, y, z, Ellipsoid::WGS84);
+                            info!(
+                                "position defined in dataset: {:?} [ECEF] (lat={:.5}°, lon={:.5}°",
+                                (x, y, z),
+                                lat,
+                                lon
+                            );
+                            Some((x, y, z))
+                        } else {
+                            // this is not a problem in basic opmodes,
+                            // but will most likely prevail advanced opmodes.
+                            // Let the user know.
+                            warn!("no RX position defined");
+                            None
+                        }
+                    },
+                }
+            },
+        })
+    }
+}
+
+impl Cli {
+    /// Build new command line interface
+    pub fn new() -> Self {
+        Self {
+            matches: {
+                Command::new("rinex-cli")
+                    .author("Guillaume W. Bres, <guillaume.bressaix@gmail.com>")
+                    .version(env!("CARGO_PKG_VERSION"))
+                    .about("RINEX post processing (command line)")
+                    .arg_required_else_help(true)
+                    .color(ColorChoice::Always)
+                    .arg(Arg::new("filepath")
+                        .short('f')
+                        .long("fp")
+                        .value_name("FILE")
+                        .action(ArgAction::Append)
+                        .required_unless_present("directory")
+                        .help("Input file. RINEX (any format, including Clock and ANTEX), and SP3 are accepted. You can load as many files as you need."))
+                    .arg(Arg::new("directory")
+                        .short('d')
+                        .long("dir")
+                        .value_name("DIRECTORY")
+                        .action(ArgAction::Append)
+                        .required_unless_present("filepath")
+                        .help("Load directory recursively. Default recursive depth is set to 5,
+but you can extend that with --depth.
+Again any RINEX, and SP3 are accepted. You can load as many directories as you need."))
+                    .arg(Arg::new("depth")
+                        .long("depth")
+                        .action(ArgAction::Set)
+                        .required(false)
+                        .value_parser(value_parser!(u8))
+                        .help("Extend maximal recursive search depth of -d. The default is 5."))
+                    .arg(Arg::new("quiet")
+                        .short('q')
+                        .long("quiet")
+                        .action(ArgAction::SetTrue)
+                        .help("Disable all terminal output. Also disables auto HTML reports opener."))
+                    .arg(Arg::new("workspace")
+                        .short('w')
+                        .long("workspace")
+                        .value_name("FOLDER")
+                        .value_parser(value_parser!(PathBuf))
+                        .help("Define custom workspace location. The env. variable RINEX_WORKSPACE, if present, is prefered.
+If none of those exist, we will generate local \"WORKSPACE\" folder."))
+        .next_help_heading("Preprocessing")
+            .arg(Arg::new("gps-filter")
+                .short('G')
+                .action(ArgAction::SetTrue)
+                .help("Filters out all GPS vehicles"))
+            .arg(Arg::new("glo-filter")
+                .short('R')
+                .action(ArgAction::SetTrue)
+                .help("Filters out all Glonass vehicles"))
+            .arg(Arg::new("gal-filter")
+                .short('E')
+                .action(ArgAction::SetTrue)
+                .help("Filters out all Galileo vehicles"))
+            .arg(Arg::new("bds-filter")
+                .short('C')
+                .action(ArgAction::SetTrue)
+                .help("Filters out all BeiDou vehicles"))
+            .arg(Arg::new("qzss-filter")
+                .short('J')
+                .action(ArgAction::SetTrue)
+                .help("Filters out all QZSS vehicles"))
+            .arg(Arg::new("irnss-filter")
+                .short('I')
+                .action(ArgAction::SetTrue)
+                .help("Filters out all IRNSS vehicles"))
+            .arg(Arg::new("sbas-filter")
+                .short('S')
+                .action(ArgAction::SetTrue)
+                .help("Filters out all SBAS vehicles"))
+            .arg(Arg::new("preprocessing")
+                .short('P')
+                .num_args(1..)
+                .action(ArgAction::Append)
+                .help("Filter designer. Refer to []."))
+            .arg(Arg::new("lli-mask")
+                .long("lli-mask")
+                .help("Applies given LLI AND() mask. 
+Also drops observations that did not come with an LLI flag"))
+            .next_help_heading("Receiver Antenna")
+                .arg(Arg::new("rx-ecef")
+                    .long("rx-ecef")
+                    .value_name("\"x,y,z\" coordinates in ECEF [m]")
+                    .help("Define the (RX) antenna position manually, in [m] ECEF.
+Especially if your dataset does not define such position. 
+Otherwise it gets automatically picked up."))
+                .arg(Arg::new("rx-geo")
+                    .long("rx-geo")
+                    .value_name("\"lat,lon,alt\" coordinates in ddeg [°]")
+                    .help("Define the (RX) antenna position manualy, in decimal degrees."))
+                .next_help_heading("Exclusive Opmodes: you can only run one at a time.")
+                .subcommand(graph::subcommand())
+                .subcommand(identify::subcommand())
+                .subcommand(merge::subcommand())
+                .subcommand(positioning::subcommand())
+                .subcommand(qc::subcommand())
+                .subcommand(split::subcommand())
+                .subcommand(substract::subcommand())
+                .subcommand(time_binning::subcommand())
+                .get_matches()
+            },
+        }
+    }
+    /// Returns list of input directories
+    pub fn input_directories(&self) -> Vec<&String> {
+        if let Some(fp) = self.matches.get_many::<String>("directory") {
+            fp.collect()
+        } else {
+            Vec::new()
+        }
+    }
+    /// Returns individual input filepaths
+    pub fn input_files(&self) -> Vec<&String> {
+        if let Some(fp) = self.matches.get_many::<String>("filepath") {
+            fp.collect()
+        } else {
+            Vec::new()
+        }
+    }
+    pub fn preprocessing(&self) -> Vec<&String> {
+        if let Some(filters) = self.matches.get_many::<String>("preprocessing") {
+            filters.collect()
+        } else {
+            Vec::new()
+        }
+    }
+    pub fn gps_filter(&self) -> bool {
+        self.matches.get_flag("gps-filter")
+    }
+    pub fn glo_filter(&self) -> bool {
+        self.matches.get_flag("glo-filter")
+    }
+    pub fn gal_filter(&self) -> bool {
+        self.matches.get_flag("gal-filter")
+    }
+    pub fn bds_filter(&self) -> bool {
+        self.matches.get_flag("bds-filter")
+    }
+    pub fn qzss_filter(&self) -> bool {
+        self.matches.get_flag("qzss-filter")
+    }
+    pub fn sbas_filter(&self) -> bool {
+        self.matches.get_flag("sbas-filter")
+    }
+    pub fn irnss_filter(&self) -> bool {
+        self.matches.get_flag("irnss-filter")
+    }
+    /*
+     * faillible 3D coordinates parsing
+     * it's better to panic if the descriptor is badly format
+     * then continuing with possible other coordinates than the
+     * ones desired by user
+     */
+    fn parse_3d_coordinates(desc: &String) -> (f64, f64, f64) {
+        let content = desc.split(',').collect::<Vec<&str>>();
+        if content.len() < 3 {
+            panic!("expecting x, y and z coordinates (3D)");
+        }
+        let x = f64::from_str(content[0].trim())
+            .unwrap_or_else(|_| panic!("failed to parse x coordinates"));
+        let y = f64::from_str(content[1].trim())
+            .unwrap_or_else(|_| panic!("failed to parse y coordinates"));
+        let z = f64::from_str(content[2].trim())
+            .unwrap_or_else(|_| panic!("failed to parse z coordinates"));
+        (x, y, z)
+    }
+    fn manual_ecef(&self) -> Option<(f64, f64, f64)> {
+        let desc = self.matches.get_one::<&String>("rx-ecef")?;
+        let ecef = Self::parse_3d_coordinates(desc);
+        Some(ecef)
+    }
+    fn manual_geodetic(&self) -> Option<(f64, f64, f64)> {
+        let desc = self.matches.get_one::<&String>("rx-geo")?;
+        let geo = Self::parse_3d_coordinates(desc);
+        Some(geo)
+    }
+    /// Returns RX Position possibly specified by user
+    pub fn manual_position(&self) -> Option<(f64, f64, f64)> {
+        if let Some(position) = self.manual_ecef() {
+            Some(position)
+        } else if let Some((lat, lon, alt)) = self.manual_geodetic() {
+            Some(geodetic2ecef(lat, lon, alt, Ellipsoid::WGS84))
+        } else {
+            None
+        }
+    }
+}

commit 77f85c51beb044b6f087db7e5831469011b2348b

Fix rinex-cli commands (#213)
* Correct issues with arguments to rinex-cli's identify command

Fixes several errors that are encountered when trying to use the identify
command:

  - The "all" and "anomalies" arguments were using the same short
    option identifier 'a'. The former has been renamed to 'A'.

  - The "header" argument had a short option identifier 'h' that
    conflicted with the implicit "help". The short option has been
    removed for "header".

  - The "station" argument was duplicated in its entirety

Fixes: 6fc919091c13

* Correct issues with arguments to rinex-cli's graph command

A runtime error is encountered when trying to use this command because
there is no "no-graph" option anymore that these commands conflict with.

Fixes: 6fc919091c13

* Correct issue with using rx-ecef and rx-geo options

Rust panics with a variation of following error when trying to pass
either of these options to rinex-cli:

~~~
Mismatch between definition and access of `rx-ecef`. Could not downcast to
&alloc::string::String, need to downcast to alloc::string::String
~~~

Fixes: 6fc919091c13

rinex-cli/src/cli/graph.rs
@@ -55,7 +55,6 @@ When METEO RINEX is provided, data from meteo sensors is plotted too.",
                 .long("gf")
                 .short('g')
                 .action(ArgAction::SetTrue)
-                .conflicts_with("no-graph")
                 .help("Plot Geometry Free (GF) signal combination."),
         )
         .arg(
@@ -70,7 +69,6 @@ When METEO RINEX is provided, data from meteo sensors is plotted too.",
                 .long("nl")
                 .short('n')
                 .action(ArgAction::SetTrue)
-                .conflicts_with("no-graph")
                 .help("Plot Narrow Lane (WL) signal combination."),
         )
         .arg(
@@ -78,7 +76,6 @@ When METEO RINEX is provided, data from meteo sensors is plotted too.",
                 .long("mw")
                 .short('m')
                 .action(ArgAction::SetTrue)
-                .conflicts_with("no-graph")
                 .help("Plot Melbourne-Wübbena (MW) signal combination."),
         )
         .arg(Arg::new("cs").long("cs").action(ArgAction::SetTrue).help(

rinex-cli/src/cli/identify.rs
@@ -8,7 +8,7 @@ pub fn subcommand() -> Command {
         .arg_required_else_help(true)
         .about("RINEX data identification opmode")
         .arg(
-            Arg::new("all").short('a').action(ArgAction::SetTrue).help(
+            Arg::new("all").short('A').action(ArgAction::SetTrue).help(
                 "Complete RINEX dataset(s) identification. Turns on all following algorithms.",
             ),
         )
@@ -36,7 +36,6 @@ pub fn subcommand() -> Command {
         .arg(
             Arg::new("header")
                 .long("header")
-                .short('h')
                 .action(ArgAction::SetTrue)
                 .help("Extracts major header fields"),
         )
@@ -75,12 +74,6 @@ pub fn subcommand() -> Command {
                 .help("Identify Navigation frame types."),
         )
         .next_help_heading("Clock RINEX")
-        .arg(
-            Arg::new("station")
-                .long("station")
-                .action(ArgAction::SetTrue)
-                .help("Identify all ground stations contained in CLK RINEX file(s)"),
-        )
         .arg(
             Arg::new("station")
                 .long("station")

rinex-cli/src/cli/mod.rs
@@ -283,12 +283,12 @@ Otherwise it gets automatically picked up."))
         (x, y, z)
     }
     fn manual_ecef(&self) -> Option<(f64, f64, f64)> {
-        let desc = self.matches.get_one::<&String>("rx-ecef")?;
+        let desc = self.matches.get_one::<String>("rx-ecef")?;
         let ecef = Self::parse_3d_coordinates(desc);
         Some(ecef)
     }
     fn manual_geodetic(&self) -> Option<(f64, f64, f64)> {
-        let desc = self.matches.get_one::<&String>("rx-geo")?;
+        let desc = self.matches.get_one::<String>("rx-geo")?;
         let geo = Self::parse_3d_coordinates(desc);
         Some(geo)
     }