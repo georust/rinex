a400f8de2abdc776fbee6264812caf430e594e8a
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2024-06-12 20:21:11 +0200

    update html trait
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- qc-traits/src/html.rs
+++ qc-traits/src/html.rs
@@ -3,8 +3,31 @@ pub use horrorshow::{box_html, helper::doctype, html, RenderBox};
 
 /// HTML Rendering
 pub trait RenderHtml {
+    fn georust_logo_url(&self) -> &'static str {
+        "https://raw.githubusercontent.com/georust/meta/master/logo/logo.png"
+    }
     /// Renders self to plain HTML, generating a whole entity.
-    fn to_html(&self) -> String;
+    fn to_html(&self) -> String {
+        format!(
+        "{}",
+            html! {
+                : doctype::HTML;
+                html {
+                    head {
+                        meta(charset="utf-8");
+                        meta(name="viewport", content="width=device-width, initial-scale=1");
+                        link(rel="stylesheet", href="https:////cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css");
+                        link(rel="stylesheet", href="https://www.w3schools.com/w3css/4/w3.css");
+                        script(defer="true", src="https://use.fontawesome.com/releases/v5.3.1/js/all.js");
+                        link(rel="icon", src=self.georust_logo_url(), style="width:35px;height:35px;");
+                    }
+                    body {
+                        : self.to_inline_html()
+                    }
+                }
+            }
+        )
+    }
     /// Renders self as an HTML node.
     fn to_inline_html(&self) -> Box<dyn RenderBox + '_>;
 }
