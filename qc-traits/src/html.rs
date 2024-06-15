e2906c38139e94176458bd3a341e25e50d4f45cf
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2024-06-15 16:51:52 +0200

    html model
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- qc-traits/src/html.rs
+++ qc-traits/src/html.rs
@@ -14,13 +14,16 @@ pub trait RenderHtml {
                 : doctype::HTML;
                 html {
                     head {
-                        meta(charset="utf-8");
+                        meta(charset="UTF-8");
+                        meta(http-equiv="X-UA-Compatible", content="IE=edge");
                         meta(name="viewport", content="width=device-width, initial-scale=1");
-                        link(rel="icon", type="image/x-icon", href=self.georust_logo_url(), style="width:35px;height:35px;");
-                        link(rel="stylesheet", href="https:////cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css");
-                        link(rel="stylesheet", href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css");
-                        link(rel="stylesheet", href="https://www.w3schools.com/w3css/4/w3.css");
+                        link(rel="icon", type="image/x-icon", href="https://raw.githubusercontent.com/georust/meta/master/logo/logo.png");
                         script(defer="true", src="https://use.fontawesome.com/releases/v5.3.1/js/all.js");
+                        link(rel="stylesheet", href="https://cdn.jsdelivr.net/npm/bulma@1.0.0/css/bulma.min.css");
+                        link(rel="stylesheet", href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css");
+                    }
+                    title {
+                        : "RINEX QC"
                     }
                     body {
                         : self.to_inline_html()
