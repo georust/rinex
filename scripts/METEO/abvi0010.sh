727f87ebd1dcdc57856156fa6726c57d8c401f70
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2024-06-23 14:33:47 +0200

    meteo reporting concluded
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- scripts/METEO/abvi0010.sh
+++ scripts/METEO/abvi0010.sh
@@ -2,7 +2,9 @@
 # QC report example when providing a complete/modern meteo file
 DATA_DIR=test_resources
 
-# This is our only example of Wind Direction observations
+# This is our only example that contains Wind Direction + Wind speed observations.
+# When wind direction observations are present, you get a compass view.
+# When both are present, the wind direction compass is indexed on the speed observations.
 METEO=$DATA_DIR/MET/V2/abvi0010.15m
 
 # Example: complete analysis
