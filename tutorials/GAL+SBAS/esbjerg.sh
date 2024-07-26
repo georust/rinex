4c55c3ba66644edcaa130db9312b9e327c6274c0
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2024-07-26 22:46:32 +0200

    add new tutorials
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- tutorials/GAL+SBAS/esbjerg.sh
+++ tutorials/GAL+SBAS/esbjerg.sh
@@ -15,3 +15,11 @@ CONF=tutorials/config/survey/cpp_kf.json # basic CPP+Kf
     --fp $DATA_DIR/SP3/GRG0MGXFIN_20201770000_01D_15M_ORB.SP3.gz \
     --fp $DATA_DIR/CLK/V3/GRG0MGXFIN_20201770000_01D_30S_CLK.CLK.gz \
     ppp -c $CONF
+
+./target/release/rinex-cli \
+    -P $FILTER \
+    --fp $DATA_DIR/CRNX/V3/ESBC00DNK_R_20201770000_01D_30S_MO.crx.gz \
+    --fp $DATA_DIR/NAV/V3/ESBC00DNK_R_20201770000_01D_MN.rnx.gz \
+    --fp $DATA_DIR/SP3/GRG0MGXFIN_20201770000_01D_15M_ORB.SP3.gz \
+    --fp $DATA_DIR/CLK/V3/GRG0MGXFIN_20201770000_01D_30S_CLK.CLK.gz \
+    ppp --cggtts -c $CONF
