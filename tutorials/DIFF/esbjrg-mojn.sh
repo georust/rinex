d7a63d97bc02465805964bb6d5a54f9f5bc8a653
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2024-07-27 19:39:39 +0200

    fix diff example
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- tutorials/DIFF/esbjrg-mojn.sh
+++ tutorials/DIFF/esbjrg-mojn.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
-# RINEX(A) - RINEX(B) observation
-# This operation is typicall very powerful to compare two GNSS receivers
-# to another, as long as the experimental setup is meaningful (for example: sync'ed or shared clocks).
-# To perform the differentiation, we only need a common timeframe and common signals.
+# OBS_RINEX(Esbjerg) - OBS_RINEX(Mojn)
+# This operation is very powerful to compare two GNSS receivers setup.
+# Especially when two clocks are synchronized or is split in the setup.
+# To perform the differentiation, you need a common timeframe and common signals.
 # For the lack of a better example, we use MOJNDNK and ESBJRG (DNK) that provided
 # data on the same day, and happen to be very close to one another
-WORKSPACE=WORKPACE
+WORKSPACE=WORKSPACE
 DATA_DIR=test_resources/CRNX/V3
 
-FILTER=">E05;<E20" # Focus on Gal>05,<20 (any signals)
+# BDSBAS: S44
+# EDNOS : S23,S26
+#   Gal : >05
+#   GPS : <20
+FILTER="GPS,Gal;>E05;<G20"
 TIMEFRAME=">2020-06-25T02:00:00 UTC" # skip 2hr (example)
 
 # Generate ""differenced"" observation RINEX=obs(A)-obs(B)
@@ -18,6 +22,6 @@ TIMEFRAME=">2020-06-25T02:00:00 UTC" # skip 2hr (example)
     --fp $DATA_DIR/ESBC00DNK_R_20201770000_01D_30S_MO.crx.gz \
     diff $DATA_DIR/MOJN00DNK_R_20201770000_01D_30S_MO.crx.gz
 
-# Analyze differenced observations
+# differential analysis
 ./target/release/rinex-cli \
     --fp $WORKSPACE/ESBC00DNK_R_20201770000_01D_30S_MO/DIFFERENCED.crx.gz
