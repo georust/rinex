77dbb59298f04f2f4d62084eb0a780a6bef48985
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2024-07-27 17:23:46 +0200

    update scripts
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- tutorials/JMF/2024_092_PARIS/galileo.sh
+++ tutorials/JMF/2024_092_PARIS/galileo.sh
@@ -9,11 +9,13 @@
 # GDOP=200
 CONF=tutorials/JMF/2024_092_PARIS/cpp_kf.json
     
+## full ppp like context
 OBS=test_resources/OBS/V3/GEOP092I.24o.gz
 SP3=test_resources/SP3/GFZ0OPSRAP_20240920000_01D_05M_ORB.SP3.gz
 CLK=test_resources/CLK/V3/GFZ0OPSRAP_20240920000_01D_05M_CLK.CLK.gz
 NAV=test_resources/NAV/V3/BRUX00BEL_R_20240920000_01D_EN.rnx.gz
 
+# run + ppp solutions (silent)
 ./target/release/rinex-cli \
     -P Galileo \
     --fp $OBS \
@@ -21,3 +23,26 @@ NAV=test_resources/NAV/V3/BRUX00BEL_R_20240920000_01D_EN.rnx.gz
     --fp $SP3 \
     --fp $CLK \
     ppp -c $CONF
+
+# cggtts solutions (+open)
+# When using exact same input parameters: report is preserved
+# and new chapters are appended.
+# Although CGGTTS is a laboraty/advanced technique,
+# we can deploy it on any context (this is just an example).
+
+# Such a short observation period is incompatible with standard cggtts,
+# we need to move on to special tracking.
+TRACKING="1 min"
+
+# customization example
+PRODUCER="JMF"
+CLOCK="ANDROID"
+
+./target/release/rinex-cli \
+    -P Galileo \
+    --fp $OBS \
+    --fp $NAV \
+    --fp $SP3 \
+    --fp $CLK \
+    ppp --cggtts -c $CONF \
+    --lab $PRODUCER --clk $CLOCK -t "$TRACKING"
