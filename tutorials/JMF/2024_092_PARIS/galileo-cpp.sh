75af8efa3442f01a2d18c065b5a606d3b5772f29
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2024-07-27 11:10:38 +0200

    add scripts
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- tutorials/JMF/2024_092_PARIS/galileo-cpp.sh
+++ tutorials/JMF/2024_092_PARIS/galileo-cpp.sh
@@ -1,6 +1,14 @@
 #! /bin/sh
-# Post Processed (+3 week SP3/CLK) analysis (only)
+# Post Processed (+3 week SP3/CLK) surveying
 
+# Custom config: GDOP we're obtaining from Galileo is incompatible
+#   with our default config/survey configs (Mobile Phone?)
+# CPP; x17; 
+# filter=kalman;
+# System=GPST
+# GDOP=200
+CONF=tutorials/JMF/2024_092_PARIS/cpp_kf.json
+    
 OBS=test_resources/OBS/V3/GEOP092I.24o.gz
 SP3=test_resources/SP3/GFZ0OPSRAP_20240920000_01D_05M_ORB.SP3.gz
 CLK=test_resources/CLK/V3/GFZ0OPSRAP_20240920000_01D_05M_CLK.CLK.gz
@@ -11,4 +19,5 @@ NAV=test_resources/NAV/V3/BRUX00BEL_R_20240920000_01D_EN.rnx.gz
     --fp $OBS \
     --fp $NAV \
     --fp $SP3 \
-    --fp $CLK
+    --fp $CLK \
+    ppp -c $CONF
