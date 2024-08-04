725ad92885c7416598abadb2f8d1536a89cfb975
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2024-08-04 19:51:11 +0200

    update examples
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- tutorials/GAL/esbjerg-brdc.sh
+++ tutorials/GAL/esbjerg-brdc.sh
@@ -21,7 +21,7 @@ CONF=tutorials/config/survey/cpp_kf.json
 #   -o: custom name
 ./target/release/rinex-cli \
     -P $FILTER "$TIMEFRAME" \
-    -f -o "BRDC-GalE1E5" \
+    -f -q -o "BRDC-GalE1E5" \
     --fp $DATA_DIR/CRNX/V3/ESBC00DNK_R_20201770000_01D_30S_MO.crx.gz \
     --fp $DATA_DIR/NAV/V3/ESBC00DNK_R_20201770000_01D_MN.rnx.gz \
     ppp -c $CONF
@@ -30,3 +30,9 @@ CONF=tutorials/config/survey/cpp_kf.json
 # Since we're using strict identical options,
 # the report is preserved and new solutions are appended.
 # The report is automatically opened.
+./target/release/rinex-cli \
+    -P $FILTER "$TIMEFRAME" \
+    -o "BRDC-GalE1E5" \
+    --fp $DATA_DIR/CRNX/V3/ESBC00DNK_R_20201770000_01D_30S_MO.crx.gz \
+    --fp $DATA_DIR/NAV/V3/ESBC00DNK_R_20201770000_01D_MN.rnx.gz \
+    ppp --cggtts -c $CONF
