da0d1c89f8eae6e07dc2611a1f3869ff05050dd4
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2024-08-07 12:11:47 +0200

    fix proposed tutorial
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- tutorials/GPS/esbjerg.sh
+++ tutorials/GPS/esbjerg.sh
@@ -26,16 +26,15 @@ CONF=tutorials/config/survey/cpp_kf.json
     --fp $DATA_DIR/CLK/V3/GRG0MGXFIN_20201770000_01D_30S_CLK.CLK.gz \
     ppp -c $CONF
 
-exit 0
 # cggtts solutions (+open).
 # Since we're using strict identical options,
 # the report is preserved and new solutions are appended.
 # The report is automatically opened.
-# ./target/release/rinex-cli \
-#     -P $FILTER \
-#     -o "GPS-L1L5" \
-#     --fp $DATA_DIR/CRNX/V3/ESBC00DNK_R_20201770000_01D_30S_MO.crx.gz \
-#     --fp $DATA_DIR/NAV/V3/ESBC00DNK_R_20201770000_01D_MN.rnx.gz \
-#     --fp $DATA_DIR/SP3/GRG0MGXFIN_20201770000_01D_15M_ORB.SP3.gz \
-#     --fp $DATA_DIR/CLK/V3/GRG0MGXFIN_20201770000_01D_30S_CLK.CLK.gz \
-#     ppp --cggtts -c $CONF
+./target/release/rinex-cli \
+    -P $FILTER \
+    -o "GPS-L1L5" \
+    --fp $DATA_DIR/CRNX/V3/ESBC00DNK_R_20201770000_01D_30S_MO.crx.gz \
+    --fp $DATA_DIR/NAV/V3/ESBC00DNK_R_20201770000_01D_MN.rnx.gz \
+    --fp $DATA_DIR/SP3/GRG0MGXFIN_20201770000_01D_15M_ORB.SP3.gz \
+    --fp $DATA_DIR/CLK/V3/GRG0MGXFIN_20201770000_01D_30S_CLK.CLK.gz \
+    ppp --cggtts -c $CONF
