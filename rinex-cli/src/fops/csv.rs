eec6e2afbb7969d274947753ea4b85f9290c1763
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2024-08-13 15:56:44 +0200

    csv export
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- rinex-cli/src/fops/csv.rs
+++ rinex-cli/src/fops/csv.rs
@@ -6,14 +6,14 @@ use std::path::Path;
 pub fn write_obs_rinex<P: AsRef<Path>>(rnx: &Rinex, path: P) -> Result<(), Error> {
     let mut w = Writer::from_path(path)?;
     w.write_record(&[
-        "Epoch ",
-        "Flag ",
-        "Clock Offset [s] ",
-        "SV ",
-        "RINEX Code ",
-        "Value ",
-        "LLI ",
-        "SNR ",
+        "Epoch",
+        "Flag",
+        "Clock Offset [s]",
+        "SV",
+        "RINEX Code",
+        "Value",
+        "LLI",
+        "SNR",
     ])?;
     for ((epoch, flag), (clk, svnn)) in rnx.observation() {
         let t = epoch.to_string();
@@ -45,21 +45,38 @@ pub fn write_obs_rinex<P: AsRef<Path>>(rnx: &Rinex, path: P) -> Result<(), Error
     Ok(())
 }
 
-pub fn write_nav_rinex<P: AsRef<Path>>(obs: &Rinex, brdc: &Rinex, path: P) -> Result<(), Error> {
-    let mut w = Writer::from_path(path)?;
-    w.write_record(&["Epoch", "SV", "x_ecef_km", "y_ecef_km", "z_ecef_km"])?;
+pub fn write_nav_rinex(obs: &Rinex, brdc: &Rinex, path: &Path) -> Result<(), Error> {
+    let mut orbit_w = Writer::from_path(path)?;
+    orbit_w.write_record(&["Epoch", "SV", "x_ecef_km", "y_ecef_km", "z_ecef_km"])?;
+
+    let parent = path.parent().unwrap();
+    let stem = path.file_stem().unwrap().to_string_lossy().to_string();
+
+    let clk_path = parent.join(&format!("{}-clock.csv", stem));
+    let mut clk_w = Writer::from_path(clk_path)?;
+    clk_w.write_record(&["Epoch", "SV", "correction"])?;
+
     for ((t, _), (_, svnn)) in obs.observation() {
         let t_str = t.to_string();
         for (sv, _) in svnn.iter() {
             let sv_str = sv.to_string();
-            if let Some((x_ecef_km, y_ecef_km, z_ecef_km)) = brdc.sv_position(*sv, *t) {
-                w.write_record(&[
-                    &t_str,
-                    &sv_str,
-                    &format!("{:.3E} ", x_ecef_km),
-                    &format!("{:.3E} ", y_ecef_km),
-                    &format!("{:.3E} ", z_ecef_km),
-                ])?;
+            if let Some((toc, toe, eph)) = brdc.sv_ephemeris(*sv, *t) {
+                if let Some((x_ecef_km, y_ecef_km, z_ecef_km)) = eph.kepler2position(*sv, toc, *t) {
+                    orbit_w.write_record(&[
+                        &t_str,
+                        &sv_str,
+                        &format!("{:.3E}", x_ecef_km),
+                        &format!("{:.3E}", y_ecef_km),
+                        &format!("{:.3E}", z_ecef_km),
+                    ])?;
+                    if let Some(correction) = eph.clock_correction(toc, *t, *sv, 8) {
+                        clk_w.write_record(&[
+                            &t_str,
+                            &sv_str,
+                            &format!("{:.3E}", correction.to_seconds()),
+                        ])?;
+                    }
+                }
             }
         }
     }
