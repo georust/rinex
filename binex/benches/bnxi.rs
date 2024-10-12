9b8c052f3f9f54ce86845158d2f2dce60db42f8d
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2024-10-13 00:49:09 +0200

    add bnx00 benchmark
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

---
+++ binex/benches/bnxi.rs
@@ -0,0 +1,29 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use binex::prelude::{Epoch, Message, MonumentGeoMetadata, MonumentGeoRecord, Record, TimeResolution};
+
+#[allow(unused_must_use)]
+pub fn criterion_benchmark(c: &mut Criterion) {
+
+    let mut buf = [0; 128];
+    let t0 = Epoch::from_gpst_seconds(10.0);
+    let meta = MonumentGeoMetadata::RNX2BIN;
+    
+    let record = MonumentGeoRecord::new(t0, meta)   
+        .with_comment("This is a test")
+        .with_climatic_info("basic info")
+        .with_geophysical_info("another field")
+        .with_user_id("Test");
+
+    let record = Record::new_monument_geo(record);
+    let msg = Message::new(true, TimeResolution::QuarterSecond, false, false, record);
+
+    c.bench_function("bnx00", |b| {
+        b.iter(|| {
+            black_box(msg.encode(&mut buf));
+            black_box(Message::decode(&buf));
+        })
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
