d53470072f5e86dd659da940641b29b2466068ef
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2024-11-24 20:10:11 +0100

    update docs
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- qc-traits/src/processing/split.rs
+++ qc-traits/src/processing/split.rs
@@ -3,12 +3,20 @@ use hifitime::{Duration, Epoch};
 
 /// Implement [Split] to rearrange datasets timewise.
 pub trait Split {
+
+    /// [Split]s Self into two at specified [Epoch]
+    /// Returns:
+    ///  - (a , b) where a <= t and b > t
     fn split(&self, t: Epoch) -> (Self, Self)
     where
         Self: Sized;
 
+    /// [Split]s Self with mutable access.
+    /// Modifies Self in place, retaining only <= t.
+    /// Returns > t.
     fn split_mut(&mut self, t: Epoch) -> Self;
 
+    /// [Split]s Self into chunks of evenly spaced [Duration]
     fn split_even_dt(&self, dt: Duration) -> Vec<Self>
     where
         Self: Sized;
