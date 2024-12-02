94c3617f90cf6c8e9e8c33365ffe61b91d4018c1
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2024-12-02 17:32:13 +0100

    RNX2BIN
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- rinex/src/binex/rnx2bin.rs
+++ rinex/src/binex/rnx2bin.rs
@@ -1,28 +1,57 @@
 //! RINEX to BINEX serialization
 use std::io::Write;
-
 use crate::prelude::Rinex;
-
 use binex::prelude::{Encoder, Message};
 
-/// RNX2BIN can serialize a RINEX to a stream of BINEX [Message]s
-pub struct RNX2BIN<W: Write> {
-    /// BINEX encoder
-    encoder: Encoder<W>,
+/// RNX2BIN can serialize a [Rinex]
+/// into a stream of BINEX [Message]s
+pub struct RNX2BIN<&'a> {
+    /// [Rinex] that was previously collected
+    rinex: &'a Rinex,
+    /// State to consume the header
+    state: State,
 }
 
-impl<W: Write> Iterator for RNX2BIN<W> {
+#[derive(Debug, Default, Clone, Copy, PartialEq)]
+enum State {
+    /// Describes the RINEX format, Constellation and revision
+    #[default]
+    VersionType, 
+    /// Describes the RINEX original author, agency and operator
+    ProgRunBy,
+    /// Describes all comments originally written in header section
+    HeaderComments,
+    /// Done parsing Header: streaming record content
+    Record,
+}
+
+impl<'a> Iterator for RNX2BIN<'a> {
     type Item = Option<Message>;
+    /// Consume [Rinex] into [Message] stream
     fn next(&mut self) -> Option<Self::Item> {
-        None
+        match self.header_state {
+            State::VersionType => {
+
+            },
+            State::ProgRunBy => {
+
+            },
+            State::HeaderComments => {
+
+            },
+            State::Record => {
+
+            },
+        }
     }
 }
 
 impl<W: Write> RNX2BIN<W> {
     /// Creates a new [RNX2BIN].
-    pub fn new(w: W) -> Self {
+    pub(crate) fn new(rinex: &'a Rinex) -> Self {
         Self {
-            encoder: Encoder::new(w),
+            rinex,
+            header_state: State::default(),
         }
     }
 }
