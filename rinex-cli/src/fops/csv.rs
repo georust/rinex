84ff35c22f116a2807467203a60978f1f11c6397
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2024-12-11 11:02:38 +0100

    increase observation precision in formatted csv
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- rinex-cli/src/fops/csv.rs
+++ rinex-cli/src/fops/csv.rs
@@ -27,7 +27,7 @@ pub fn write_obs_rinex<P: AsRef<Path>>(rnx: &Rinex, path: P) -> Result<(), Error
             let sv = sv.to_string();
             for (code, obs) in obsnn.iter() {
                 let code = code.to_string();
-                let value = format!("{:.3E}", obs.obs);
+                let value = format!("{:.12E}", obs.obs);
                 let lli = if let Some(lli) = obs.lli {
                     format!("{:?}", lli)
                 } else {
