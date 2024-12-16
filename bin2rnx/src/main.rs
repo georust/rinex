bc800c9693c678e8ef214677fe6eeb2cc7b57d28
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2024-12-16 17:27:21 +0100

    BIN2RNX
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- bin2rnx/src/main.rs
+++ bin2rnx/src/main.rs
@@ -3,18 +3,22 @@ use cli::Cli;
 
 use log::{debug, info};
 
-use rinex::prelude::{binex::RNX2BIN, FormattingError, ParsingError, Rinex};
-
 use std::{
     fs::File,
     io::Write,
     path::{Path, PathBuf},
+    str::FromStr,
 };
 
 use flate2::{write::GzEncoder, Compression};
 use thiserror::Error;
 
-/// Supported output types
+use rinex::{
+    prelude::{binex::BIN2RNX, Duration},
+    production::{Postponing, SnapshotMode},
+};
+
+// Supported output types
 pub enum Output {
     // Simple file
     File(File),
@@ -38,81 +42,6 @@ impl Write for Output {
     }
 }
 
-impl Output {
-    pub fn new(
-        rinex: &Rinex,
-        gzip_in: bool,
-        workspace: &Path,
-        gzip_out: bool,
-        short_name: bool,
-        io_output: bool,
-        custom_name: Option<&String>,
-    ) -> Self {
-        if let Some(custom) = custom_name {
-            // custom output specified
-            if io_output {
-                // this must be a fullpath: workspace is disregarded
-                let mut fd = File::open(custom)
-                    .unwrap_or_else(|e| panic!("Failed to create file within workspace"));
-
-                if gzip_out {
-                    println!("Gzip streaming to I/O interface: {}", custom);
-                    let mut fd = GzEncoder::new(fd, Compression::new(5));
-                    Output::GzipIo(fd)
-                } else {
-                    println!("Streaming to I/O interface: {}", custom);
-                    Output::Io(fd)
-                }
-            } else {
-                let path = workspace.join(custom);
-
-                let mut fd = File::create(&path)
-                    .unwrap_or_else(|e| panic!("Failed to create file within workspace"));
-
-                if gzip_in || gzip_out {
-                    println!("Generating custom gzip file: {}", path);
-                    let mut fd = GzEncoder::new(fd, Compression::new(5));
-                    Output::GzipFile(fd)
-                } else {
-                    println!("Generating custom file: {}", path);
-                    Output::File(fd)
-                }
-            }
-        } else {
-            // auto generated name
-            let mut suffix = ".bin".to_string();
-
-            if gzip_in || gzip_out {
-                suffix.push_str(".gz");
-            }
-
-            let auto = rinex.standard_filename(short_name, Some(suffix), None);
-
-            let path = workspace.join(auto);
-
-            let mut fd = File::create(&path)
-                .unwrap_or_else(|e| panic!("Failed to create file within workspace"));
-
-            if gzip_in || gzip_out {
-                println!("Generating gzip file: {}", path);
-                let mut fd = GzEncoder::new(fd, Compression::new(5));
-                Output::GzipFile(fd)
-            } else {
-                println!("Generating file: {}", path);
-                Output::File(fd)
-            }
-        }
-    }
-}
-
-#[derive(Debug, Error)]
-enum Error {
-    #[error("parsing error")]
-    ParsingError(#[from] ParsingError),
-    #[error("formatting error")]
-    FormattingError(#[from] FormattingError),
-}
-
 fn workspace(cli: &Cli) -> PathBuf {
     if let Some(workspace) = cli.workspace() {
         Path::new(workspace).to_path_buf()
@@ -123,24 +52,25 @@ fn workspace(cli: &Cli) -> PathBuf {
     }
 }
 
-//fn binex_streaming<W: Write>(output: Output<W>, rnx2bin: &mut RNX2BIN) {
-//    const BUF_SIZE: usize = 4096;
-//    let mut buf = [0; BUF_SIZE];
-//
-//    while let Some(msg) = rnx2bin.next() {
-//        msg.encode(&mut buf, BUF_SIZE)
-//            .unwrap_or_else(|e| panic!("BINEX encoding error: {:?}", e));
-//
-//        buf = [0; BUF_SIZE];
-//
-//        output
-//            .write(&buf)
-//            .unwrap_or_else(|e| panic!("I/O error: {}", e));
-//    }
-//}
-
-fn main() -> Result<(), Error> {
+fn main() {
     let cli = Cli::new();
 
-    Ok(())
+    let fp = cli.input_path();
+    let mut fd = File::open(fp).unwrap_or_else(|e| panic!("failed to open file interface"));
+
+    let mut bin2rnx = BIN2RNX::new_periodic(
+        false,
+        Duration::from_str("30s").unwrap(),
+        Postponing::None,
+        fd,
+    );
+
+    info!("RINEX collection starting");
+    loop {
+        println!("active: {}", bin2rnx.active);
+
+        println!("collected: {:?}", bin2rnx.nav_rinex());
+
+        std::thread::sleep(std::time::Duration::from_secs(10));
+    }
 }
