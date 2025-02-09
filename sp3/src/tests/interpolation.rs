a755e69bfc650103f97962ae14c93ac40c5a05f7
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2025-02-09 11:10:48 +0100

    upgrading interpolation api
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- sp3/src/tests/interpolation.rs
+++ sp3/src/tests/interpolation.rs
@@ -2,6 +2,7 @@
 #[cfg(test)]
 mod test {
     use crate::prelude::*;
+    use hifitime::Unit;
     use std::path::PathBuf;
     use std::str::FromStr;
 
@@ -17,6 +18,26 @@ mod test {
     // }
 
     #[test]
+    #[should_panic]
+    #[cfg(feature = "flate2")]
+    fn even_interpolation_order() {
+        let path = PathBuf::new()
+            .join(env!("CARGO_MANIFEST_DIR"))
+            .join("..")
+            .join("test_resources")
+            .join("SP3")
+            .join("EMR0OPSULT_20232391800_02D_15M_ORB.SP3.gz");
+
+        let sp3 = SP3::from_gzip_file(&path).unwrap();
+
+        let g01 = SV::from_str("G01").unwrap();
+        let t0 = Epoch::from_str("2023-08-27T18:00:00 GPST").unwrap();
+        
+        let _ = sp3
+            .satellite_position_lagrangian_interpolation(g01, t0, 2);
+    }
+
+    #[test]
     #[cfg(feature = "flate2")]
     fn interpolation_feasibility() {
         let path = PathBuf::new()
@@ -26,86 +47,64 @@ mod test {
             .join("SP3")
             .join("EMR0OPSULT_20232391800_02D_15M_ORB.SP3.gz");
 
-        let sp3 = SP3::from_gzip_file(&path);
-
-        assert!(
-            sp3.is_ok(),
-            "failed to parse EMR0OPSULT_20232391800_02D_15M_ORB.SP3.gz"
-        );
-
-        let sp3 = sp3.unwrap();
+        let sp3 = SP3::from_gzip_file(&path).unwrap();
 
         let g01 = SV::from_str("G01").unwrap();
         let g72 = SV::from_str("G72").unwrap();
 
         let t0 = Epoch::from_str("2023-08-27T18:00:00 GPST").unwrap();
         let t0_5m = Epoch::from_str("2023-08-27T18:05:00 GPST").unwrap();
+        let t0_14m_59 = Epoch::from_str("2023-08-27T18:14:59 GPST").unwrap();
+
         let t1 = Epoch::from_str("2023-08-27T18:15:00 GPST").unwrap();
+        let t1_1s = t1 + 1.0 * Unit::Second;
+
         let t2 = Epoch::from_str("2023-08-27T18:30:00 GPST").unwrap();
         let t2_10m = Epoch::from_str("2023-08-27T18:40:00 GPST").unwrap();
+
         let tn2 = Epoch::from_str("2023-08-29T17:15:00 GPST").unwrap();
         let tn2_10m = Epoch::from_str("2023-08-29T17:25:00 GPST").unwrap();
+
         let tn1 = Epoch::from_str("2023-08-29T17:30:00 GPST").unwrap();
         let tn1_10m = Epoch::from_str("2023-08-29T17:40:00 GPST").unwrap();
+
         let tn = Epoch::from_str("2023-08-29T17:45:00 GPST").unwrap();
 
         // test: invalid SV
         assert!(sp3
             .satellite_position_lagrangian_interpolation(g72, t0, 7)
-            .is_err());
+            .is_none());
 
         assert!(sp3
             .satellite_position_lagrangian_interpolation(g72, t1, 7)
-            .is_err());
+            .is_none());
 
         assert!(sp3
             .satellite_position_lagrangian_interpolation(g72, t2, 7)
-            .is_err());
-
-        assert!(sp3
-            .satellite_position_lagrangian_interpolation(g72, t2_10m, 2)
-            .is_err());
-
-        assert!(sp3
-            .satellite_position_lagrangian_interpolation(g72, tn, 2)
-            .is_err());
-
-        // test: even order
-        let even_err = sp3
-            .satellite_position_lagrangian_interpolation(g72, t0, 2)
-            .err();
-        match even_err {
-            Some(Error::EvenInterpolationOrder) => {},
-            _ => panic!("invalid error"),
-        }
+            .is_none());
 
         // test: too early (x3)
-        for t in [t0, t0_5m] {
-            let error = sp3
-                .satellite_position_lagrangian_interpolation(g01, t, 3)
-                .err();
-            match error {
-                Some(Error::InterpolationWindow) => {},
-                _ => panic!("invalid error"),
-            }
-        }
-
-        // test: too late (x3)
-        for t in [tn] {
-            let error = sp3
-                .satellite_position_lagrangian_interpolation(g01, t, 3)
-                .err();
-            match error {
-                Some(Error::InterpolationWindow) => {},
-                _ => panic!("invalid error"),
-            }
-        }
-
-        // test: feasible (x3)
-        for t in [t1] {
+        for t in [t0, t0_5m, t0_14m_59] {
             assert!(sp3
                 .satellite_position_lagrangian_interpolation(g01, t, 3)
-                .is_ok());
+                .is_none());
         }
+
+        // test: first x3 feasible
+        assert!(sp3.satellite_position_lagrangian_interpolation(g01, t1, 3).is_some()); 
+
+        // // test: too late (x3)
+        // for t in [tn] {
+        //     assert!(sp3
+        //         .satellite_position_lagrangian_interpolation(g01, t, 3)
+        //         .is_none());
+        // }
+
+        // // test: feasible (x3)
+        // for t in [t1] {
+        //     assert!(sp3
+        //         .satellite_position_lagrangian_interpolation(g01, t, 3)
+        //         .is_some());
+        // }
     }
 }
