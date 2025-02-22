08f1c6a1f5688c2710f2865aea8c7e9a06bfc21a
gwbres <guillaume.bressaix@gmail.com>
2025-02-22 13:45:41 +0100

    Introduce RINEX benchmarking (#301)
    * Introduce RINEX benchmarking
    ---------
    
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

---
+++ rinex/benches/formatting.rs
@@ -0,0 +1,66 @@
+//! Benchmarking RINEX parsing & formatting
+//! using most common format and tiny file
+extern crate criterion;
+use criterion::{criterion_group, criterion_main, Criterion};
+
+use std::io::{BufWriter, Write};
+
+use rinex::prelude::Rinex;
+
+#[derive(Debug)]
+pub struct Utf8Buffer {
+    pub inner: Vec<u8>,
+}
+
+impl Write for Utf8Buffer {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        for b in buf {
+            self.inner.push(*b);
+        }
+        Ok(buf.len())
+    }
+    fn flush(&mut self) -> std::io::Result<()> {
+        self.inner.clear();
+        Ok(())
+    }
+}
+
+impl Utf8Buffer {
+    pub fn new(capacity: usize) -> Self {
+        Self {
+            inner: Vec::with_capacity(capacity),
+        }
+    }
+}
+
+fn rinex_formatting<W: Write>(rinex: &Rinex, w: &mut BufWriter<W>) {
+    let _ = rinex.format(w).unwrap();
+}
+
+fn benchmark(c: &mut Criterion) {
+    let mut formatting_grp = c.benchmark_group("formatting");
+
+    let mut buffer = BufWriter::new(Utf8Buffer::new(4096));
+
+    // Small RINEX OBS (V2)
+    let rinex = Rinex::from_file("../test_resources/OBS/V2/rovn0010.21o").unwrap();
+
+    formatting_grp.bench_function("OBS/V2", |b| {
+        b.iter(|| {
+            rinex_formatting(&rinex, &mut buffer);
+        })
+    });
+
+    // Small RINEX OBS (V3)
+    let rinex = Rinex::from_file("../test_resources/OBS/V3/DUTH0630.22O").unwrap();
+
+    formatting_grp.bench_function("OBS/V3", |b| {
+        b.iter(|| {
+            rinex_formatting(&rinex, &mut buffer);
+        })
+    });
+    formatting_grp.finish();
+}
+
+criterion_group!(benches, benchmark);
+criterion_main!(benches);
